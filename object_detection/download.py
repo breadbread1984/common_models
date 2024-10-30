05ed482ce65093eec2528da019bbdac7e5d4ffe7
breadbread1984 <breadbread1984@163.com>
2024-10-30 12:42:09 +0800

    update

--- object_detection/download.py
+++ object_detection/download.py
@@ -4,9 +4,10 @@ from absl import flags, app
 from abc import ABC, abstractmethod
 from shutil import rmtree
 from os import mkdir
-from os.path import join, exists
+from os.path import join, exists, splitext
 import gdown
 import zipfile
+import csv
 from utils import voc_to_coco
 
 FLAGS = flags.FLAGS
@@ -23,11 +24,36 @@ class PostProcess(ABC):
 class SafetyHelmet(PostProcess):
   def process(self, download_path):
     target_path = join(FLAGS.output, 'safetyhelmet')
+    # 1) unzip dataset
     if not exists(target_path):
       mkdir(target_path)
       with zipfile.ZipFile(download_path, 'r') as f:
         f.extractall(target_path)
-
+    # 2) convert VOC to COCO annotation
+    coco_ann = voc_to_coco(join(target_path, 'VOC2028', 'Annotations'))
+    # 3) load list
+    label_path = join(FLAGS.output, 'safetyhelmet', 'labels')
+    if not exists(label_path):
+      mkdir(label_path)
+    for list_file in listdir(join(target_path, 'VOC2028', 'ImageSets', 'Main')):
+      stem, ext = splitext(list_file)
+      if ext != '.txt': continue
+      with open(join(target_path, 'VOC2028', 'ImageSets', 'Main', list_file)) as f:
+        sample_list = set()
+        reader = csv.reader(f)
+        for row in reader:
+          if len(row) == 0: continue
+          sample_list.add(row[0])
+      images = list(filter(lambda x: x["id"] in sample_list, coco_ann['images']))
+      image_ids = set([image['id'] for image in images])
+      annotations = list(filter(lambda x: x["image_id"] in image_ids, coco_ann['annotations']))
+      subset_ann = {
+        "images": images,
+        "annotations": annotations,
+        "categories": coco_ann['categories']
+      }
+      with open(join(label_path, stem + '.json'), 'w') as f:
+        f.write(json.dumps(subset_ann))
 
 def main(unused_argv):
   if not exists(FLAGS.output):
@@ -42,8 +68,8 @@ def main(unused_argv):
   data_info = datasets[FLAGS.dataset]
   download_file = join(FLAGS.output, FLAGS.dataset + '.download')
   if data_info['type'] == 'google drive':
-    if not exists(download_file)
-    gdown.download(data_info['url'], output = download_file)
+    if not exists(download_file):
+      gdown.download(data_info['url'], output = download_file)
     processor = data_info['postprocess']()
     processor.process(download_file)
   else:
