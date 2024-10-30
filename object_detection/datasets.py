76f37a7df237487a01113a7a12192fa1313c9eaf
breadbread1984 <breadbread1984@163.com>
2024-10-30 15:18:33 +0800

    update

---
+++ object_detection/datasets.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+from os.path import join, exists
+from abc import ABC, abstractmethod
+from torchvision.datasets import CocoDetection, wrap_dataset_for_transforms_v2
+
+class CocoDataset(ABC):
+  @abstractmethod
+  def load(self, root_path):
+    raise NotImplementedError('CocoDataset.load is not implemented!')
+
+class SafetyHelmet(CocoDataset):
+  def load(self, root_path, split = 'train', transforms = None):
+    assert split in {'train', 'val'}
+    PATHS = {
+      "train": (join(root_path, 'VOC2028', 'JPEGImages'), join(root_path, 'labels', 'train.json')),
+      "val": (join(root_path, 'VOC2028', 'JPEGImages'), join(root_path, 'labels', 'val.json'))
+    }
+    img_folder, ann_file = PATHS[split]
+    dataset = CocoDetection(img_folder, ann_file, transforms = transforms)
+    target_keys = ['boxes', 'labels', 'image_id']
+    dataset = wrap_dataset_for_transforms_v2(dataset, target_keys = target_keys)
+    if image_set == 'train':
+      ids = list()
+      for ds_idx, img_id in enumerate(dataset.ids):
+        ann_ids = dataset.coco.getAnnIds(imgIds = img_id, iscrowd = None)
+        anno = dataset.coo.loadAnns(ann_ids)
+        if not len(anno) == 0 and \
+           not all(any(o <= 1 for o in obj["bbox"][2:]) for obj in anno):
+          ids.append(ds_idx)
+      dataset = torch.utils.data.Subset(dataset, ids)
+    return dataset
+
