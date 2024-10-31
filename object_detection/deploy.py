27e196625b96306354fa4bd412bed03d53c6d66d
breadbread1984 <breadbread1984@163.com>
2024-10-31 09:56:13 +0800

    update

--- object_detection/deploy.py
+++ object_detection/deploy.py
@@ -9,24 +9,29 @@ class Detection(object):
     assert device in {'cuda', 'cpu'}
     ckpt = torch.load(ckpt_path, map_location = device)
     state_dict = ckpt['model']
-    self.model = get_model(model = model, weights_backbone = backbone_weight, num_classes = n_classes).to(device)
+    self.model = get_model(model, weights = None, weights_backbone = backbone_weight, num_classes = n_classes).to(device)
     self.model.load_state_dict(state_dict)
     self.model.eval()
-    self.trans = get_weight(backbone_weight).transforms()
-    self.device = device
+    self.trans = get_weight(backbone_weight).transforms().to(device)
   def detect(self, x):
     assert type(x) is np.ndarray
     # 1) convert to RGB
     x = x[:,:,::-1]
     # 2) add batch
-    x = torch.from_numpy(np.expand_dims(x, axis = 0), dtype = torch.float32).to(device)
+    x = np.ascontiguousarray(np.expand_dims(np.transpose(x, (2,0,1)), axis = 0))
+    x = torch.from_numpy(x).to(torch.float32).to(next(self.model.parameters()).device)
     # 3) preprocess
     x = self.trans(x)
     # 4) predict
     outputs = self.model(x)
     outputs = [{k: v.to('cpu') for k, v in t.items()} for t in outputs]
-    import pdb; pdb.set_trace()
+    return outputs[0]
 
 if __name__ == "__main__":
   detection = Detection()
-
+  import cv2
+  img = cv2.imread('datasets/safetyhelmet/VOC2028/JPEGImages/PartB_00161.jpg')
+  objs = detection.detect(img)
+  boxes, scores, labels = objs['boxes'], objs['scores'], objs['labels']
+  for box, score, label in zip(boxes, scores, labels):
+    import pdb; pdb.set_trace()
