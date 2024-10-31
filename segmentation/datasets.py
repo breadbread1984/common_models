1ec0b56de5af2ca5db1c8edabfd78240aa5376cd
breadbread1984 <breadbread1984@163.com>
2024-10-31 15:53:44 +0800

    update

--- segmentation/datasets.py
+++ segmentation/datasets.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from abc import ABC, abstractmethod
+import zipfile
 import gdown
 
 class SegmentDataset(ABC):
@@ -14,4 +15,13 @@ class SegmentDataset(ABC):
 class iSAID(SegmentDataset):
   def process(self, download_path, output = 'datasets'):
     target_path = join(output, 'isaid')
-    # 1)
+    # 1) unzip dataset
+    with zipfile.ZipFile(join(download_path, 'train', 'Instance_masks', 'images.zip'), 'r') as f:
+      f.extractall(join(download_path, 'train', 'Instance_masks'))
+    with zipfile.ZipFile(join(download_path, 'train', 'Semantic_masks', 'images.zip'), 'r') as f:
+      f.extractall(join(download_path, 'train', 'Semantic_masks'))
+    with zipfile.ZipFile(join(download_path, 'val', 'Instance_masks', 'images.zip'), 'r') as f:
+      f.extractall(join(download_path, 'val', 'Instance_masks'))
+    with zipfile.ZipFile(join(download_path, 'val', 'Semantic_masks', 'images.zip'), 'r') as f:
+      f.extractall(join(download_path, 'val', 'Semantic_masks'))
+    # 2) 
