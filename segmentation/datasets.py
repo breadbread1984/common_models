96ba44c5727d1b39ed8103d33925bc8784a665fe
breadbread1984 <breadbread1984@163.com>
2024-11-01 15:06:48 +0800

    update

--- segmentation/datasets.py
+++ segmentation/datasets.py
@@ -4,6 +4,7 @@ from abc import ABC, abstractmethod
 from os.path import join, exists
 import zipfile
 import gdown
+from torchvision.datasets import CocoDetection
 
 class SegmentDataset(ABC):
   @abstractmethod
@@ -17,6 +18,14 @@ class iSAID(SegmentDataset):
   def process(self, download_path, output = 'datasets'):
     target_path = join(output, 'isaid')
     # 1) unzip dataset
+    with zipfile.ZipFile(join(download_path, 'train_image', 'part1.zip'), 'r') as f:
+      f.extractall(join(download_path, 'train_image'))
+    with zipfile.ZipFile(join(download_path, 'train_image', 'part2.zip'), 'r') as f:
+      f.extractall(join(download_path, 'train_image'))
+    with zipfile.ZipFile(join(download_path, 'train_image', 'part3.zip'), 'r') as f:
+      f.extractall(join(download_path, 'train_image'))
+    with zipfile.ZipFile(join(download_path, 'val_image', 'part1.zip'), 'r') as f:
+      f.extractall(join(download_path, 'val_image'))
     with zipfile.ZipFile(join(download_path, 'train', 'Instance_masks', 'images.zip'), 'r') as f:
       f.extractall(join(download_path, 'train', 'Instance_masks'))
     with zipfile.ZipFile(join(download_path, 'train', 'Semantic_masks', 'images.zip'), 'r') as f:
@@ -25,6 +34,21 @@ class iSAID(SegmentDataset):
       f.extractall(join(download_path, 'val', 'Instance_masks'))
     with zipfile.ZipFile(join(download_path, 'val', 'Semantic_masks', 'images.zip'), 'r') as f:
       f.extractall(join(download_path, 'val', 'Semantic_masks'))
-    # 2)
-  def load(self, root_path):
-    pass
+  def load(self, root_path, split = 'train', transforms = None):
+    assert split in {'train', 'val'}
+    PATHS = {
+      "train": (join(root_path, "train_image"), join(root_path, 'train', 'Annotations', 'iSAID_train.json')),
+      "val": (join(root_path, "val_image"), join(root_path, "val", "Annotations", "iSAID_val.json"))
+    }
+    img_folder, ann_file = PATHS[split]
+    dataset = CocoDetection(img_folder, ann_file, transforms = transforms)
+    if split == 'train':
+      ids = list()
+      for ds_idx, img_id in enumerate(dataset.ids):
+        ann_ids = dataset.coco.getAnnIds(imgIds = img_id, iscrowd = None)
+        anno = dataset.coco.loadAnns(ann_ids)
+        if not len(anno) == 0 and \
+           sum(obj["area"] for obj in anno) > 1000:
+          ids.append(ds_idx)
+      dataset = torch.utils.data.Subset(dataset, ids)
+    return dataset
