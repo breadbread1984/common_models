298c9312d210a8abaa674b5c495a9912fdd790d8
breadbread1984 <breadbread1984@163.com>
2024-11-11 14:43:57 +0800

    update

--- chat_model/models.py
+++ chat_model/models.py
@@ -4,7 +4,7 @@ import requests
 import json
 from langchain.llms.base import LLM
 
-def TGI(host = 'http://localhost:8080/v1/chat/completions'):
+def TGI(host = 'http://localhost:8080/generate'):
   class TGILLM(LLM):
     def __init__(self, host):
       super().__init__()
@@ -12,7 +12,7 @@ def TGI(host = 'http://localhost:8080/v1/chat/completions'):
       self.headers = {'Authorization': "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"}
     def _call(self, prompt, stop = None, run_manager = None, **kwargs):
       messages = json.loads(prompt)
-      data = {"model": "tgi", "messages": messages, "stream": true}
+      data = {"inputs": prompt, "parameters": {"temperature": 0.6, "top_p": 0.9}}
       for i in range(10):
         response = requests.post(self.url, headers = self.headers, json = data)
         if response.status_code == 200:
