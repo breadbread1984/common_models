0feb33defb4663117c8007d18853acf1fed0d2d5
breadbread1984 <breadbread1984@163.com>
2024-11-11 15:38:25 +0800

    update

--- chat_model/main.py
+++ chat_model/main.py
@@ -16,12 +16,12 @@ def create_interface():
   def chatbot_response(user_input, history):
     response = chain.invoke({'input': user_input})
     history.append((user_input, response))
-    return history, history
+    return history, history, ""
   with gr.Blocks() as demo:
     state = gr.State([])
     with gr.Row(equal_height = True):
       with gr.Column(scale = 15):
-        gr.Markdown("<h1><center>Electrolyte Agent</center></h1>")
+        gr.Markdown("<h1><center>对话系统</center></h1>")
     with gr.Row():
       with gr.Column(scale = 4):
         chatbot = gr.Chatbot(height = 450, show_copy_button = True)
@@ -32,7 +32,7 @@ def create_interface():
           clear_btn = gr.ClearButton(components = [chatbot, state], value = "清空问题")
       submit_btn.click(chatbot_response,
                        inputs = [user_input, state],
-                       outputs = [chatbot, state])
+                       outputs = [chatbot, state, user_input])
   return demo
 
 def main(unused_argv):
