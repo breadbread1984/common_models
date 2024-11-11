904d8ba07b3a48f219a64c2d7f87fd72ac5d2449
breadbread1984 <breadbread1984@163.com>
2024-11-11 15:00:22 +0800

    update

--- chat_model/chains.py
+++ chat_model/chains.py
@@ -3,6 +3,6 @@
 from models import TGI
 from prompts import chat_template
 
-def chat_chain():
-  chain = chat_template() | TGI()
+def chat_chain(host):
+  chain = chat_template() | TGI(host)
   return chain
