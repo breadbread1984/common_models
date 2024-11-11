578211acc3273bca3b450af800b704bee5aced1d
breadbread1984 <breadbread1984@163.com>
2024-11-11 17:18:34 +0800

    update

--- chat_model/chat_prompt_template.py
+++ chat_model/chat_prompt_template.py
@@ -6,12 +6,15 @@ from langchain_core.prompts import ChatPromptTemplate
 class HFChatPromptTemplate(ChatPromptTemplate):
   def __init__(self,
                messages,
-               model_id,
                *,
+               model_id = None,
                template_format = "f-string",
                **kwargs):
     super(HFChatPromptTemplate, self).__init__(messages, template_format = template_format, **kwargs)
     self.tokenizer = AutoTokenizer.from_pretrained(model_id)
+  @classmethod
+  def from_messages(cls, messages, model_id = None, template_format = "f-string"):
+    return cls(messages, model_id = model_id, template_format = template_format)
   def format_prompt(self, **kwargs):
     messages = self.format_message(**kwargs)
     string_messages = []
@@ -28,9 +31,9 @@ class HFChatPromptTemplate(ChatPromptTemplate):
     return self.tokenizer.apply_chat_template(messages, tokenize = False, add_generation_prompt = True)
 
 if __name__ == "__main__":
-  prompt = HFChatPromptTemplate([
+  prompt = HFChatPromptTemplate.from_messages([
     ('system', "you are a helpful AI bot. Your name is {name}"),
     ('human', '{user_input}')
-  ], 'Qwen/Qwen2.5-7B-Instruct')
+  ], model_id = 'Qwen/Qwen2.5-7B-Instruct')
   txt = prompt.invoke({'user_input': 'what is your name', 'name': 'robot test'})
   print(txt)
