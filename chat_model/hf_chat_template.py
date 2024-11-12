6786b786fc847d4f58ebe8b4491740a8d89cc700
breadbread1984 <breadbread1984@163.com>
2024-11-12 10:19:52 +0800

    update

--- chat_model/hf_chat_template.py
+++ chat_model/hf_chat_template.py
@@ -6,9 +6,11 @@ from transformers import AutoTokenizer
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 
+# NOTE: langchain-core >= 0.3.15
+
 class HFChatPromptTemplate(ChatPromptTemplate):
+  tokenizer: Any = None
   def format(self, **kwargs) -> str:
-    tokenizer = kwargs.get('tokenizer')
     messages = self.format_messages(**kwargs)
     hf_messages = []
     for m in messages:
@@ -21,7 +23,7 @@ class HFChatPromptTemplate(ChatPromptTemplate):
       else:
         raise Exception(f'Got unsupported message type: {m}')
       hf_messages.append({'role': role, 'content': m.content})
-    return tokenizer.apply_chat_template(hf_messages, tokenize = False, add_generation_prompt = True)
+    return self.tokenizer.apply_chat_template(hf_messages, tokenize = False, add_generation_prompt = True)
 
 if __name__ == "__main__":
   messages = [
@@ -29,7 +31,7 @@ if __name__ == "__main__":
     MessagesPlaceholder('chat_history'),
     ('human', '{user_input}')
   ]
-  prompt = HFChatPromptTemplate.from_messages(messages)
   tokenizer = AutoTokenizer.from_pretrained('Qwen/Qwen2.5-7B-Instruct')
-  txt = prompt.format(**{'user_input': 'what is your name', 'name': 'robot test', 'chat_history': [HumanMessage(content = 'your name is awesome!'), AIMessage(content = 'Thanks!')]}, tokenizer = tokenizer)
+  prompt = HFChatPromptTemplate(messages, tokenizer = tokenizer)
+  txt = prompt.format(**{'user_input': 'what is your name', 'name': 'robot test', 'chat_history': [HumanMessage(content = 'your name is awesome!'), AIMessage(content = 'Thanks!')]})
   print(txt)
