3eada2723d7306c10b561c785d4433f7b5a407df
breadbread1984 <breadbread1984@163.com>
2024-11-12 10:24:20 +0800

    update

--- chat_model/chains.py
+++ chat_model/chains.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 
 from models import TGI
-from prompts import chat_template
+from transformers import AutoTokenizer
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+from langchain_core.prompts import MessagesPlaceholder
+from hf_chat_template import HFChatPromptTemplate
 
 def chat_chain(host):
-  chain = chat_template() | TGI(host)
+  tokenizer = AutoTokenizer.from_pretrained('Qwen/Qwen2.5-7B-Instruct')
+  template = HFChatPromptTemplate([
+    SystemMessage(content = '你是一个海盗，用海盗应该有的强调回复用户的任何问题。'),
+    MessagesPlaceholder('chat_history'),
+    HumanMessage('{input}')
+  ], tokenizer = tokenizer)
+  chain = template | TGI(host)
   return chain
