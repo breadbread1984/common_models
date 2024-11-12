8e9f2ae3b8497d4a428f99fdc8e7894607d76628
breadbread1984 <breadbread1984@163.com>
2024-11-12 11:13:41 +0800

    update

--- chat_model/chains.py
+++ chat_model/chains.py
@@ -9,9 +9,9 @@ from hf_chat_template import HFChatPromptTemplate
 def chat_chain(host):
   tokenizer = AutoTokenizer.from_pretrained('Qwen/Qwen2.5-7B-Instruct')
   template = HFChatPromptTemplate([
-    SystemMessage(content = '你是一个海盗，用海盗应该有的腔调回复用户的任何问题。'),
+    ('system', '你是一个海盗，用海盗应该有的腔调回复用户的任何问题。'),
     MessagesPlaceholder('chat_history'),
-    HumanMessage('{input}')
+    ('human', '{input}')
   ], tokenizer = tokenizer)
   chain = template | TGI(host)
   return chain
@@ -19,9 +19,9 @@ def chat_chain(host):
 if __name__ == "__main__":
   tokenizer = AutoTokenizer.from_pretrained('Qwen/Qwen2.5-7B-Instruct')
   template = HFChatPromptTemplate([
-    SystemMessage(content = '你是一个海盗，用海盗应该有的腔调回复用户的任何问题。'),
+    ('system', '你是一个海盗，用海盗应该有的腔调回复用户的任何问题。'),
     MessagesPlaceholder('chat_history'),
-    HumanMessage('{input}')
+    ('human', '{input}')
   ], tokenizer = tokenizer)
   chain = template | Test()
   print(chain.invoke({'input':'你最喜欢的音乐是什么','chat_history':[]}))
