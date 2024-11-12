271c0b669a6d7ac4b514525323ba86b6c8f4c26e
breadbread1984 <breadbread1984@163.com>
2024-11-12 10:01:12 +0800

    update

--- chat_model/chat_prompt_template.py
+++ chat_model/chat_prompt_template.py
@@ -3,14 +3,14 @@
 from typing import Any, Union
 from collections.abc import Sequence
 from transformers import AutoTokenizer
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 
 class HFChatPromptTemplate(ChatPromptTemplate):
   def format(self, **kwargs) -> str:
     tokenizer = kwargs.get('tokenizer')
     messages = self.format_messages(**kwargs)
-    string_messages = []
+    hf_messages = []
     for m in messages:
       if isinstance(m, HumanMessage):
         role = 'user'
@@ -20,18 +20,16 @@ class HFChatPromptTemplate(ChatPromptTemplate):
         role = 'system'
       else:
         raise Exception(f'Got unsupported message type: {m}')
-    messages.append({'role': role, 'content': m.content})
-    return tokenizer.apply_chat_template(messages, tokenize = False, add_generation_prompt = True)
+      hf_messages.append({'role': role, 'content': m.content})
+    return tokenizer.apply_chat_template(hf_messages, tokenize = False, add_generation_prompt = True)
 
 if __name__ == "__main__":
   messages = [
     ('system', "you are a helpful AI bot. Your name is {name}"),
+    MessagesPlaceholder('chat_history'),
     ('human', '{user_input}')
   ]
-  prompt = ChatPromptTemplate.from_messages(messages)
-  txt = prompt.format(**{'user_input': 'what is your name', 'name': 'robot test'})
-  print(str(txt))
   prompt = HFChatPromptTemplate.from_messages(messages)
   tokenizer = AutoTokenizer.from_pretrained('Qwen/Qwen2.5-7B-Instruct')
-  txt = prompt.format(**{'user_input': 'what is your name', 'name': 'robot test'}, tokenizer = tokenizer)
+  txt = prompt.format(**{'user_input': 'what is your name', 'name': 'robot test', 'chat_history': [HumanMessage(content = 'your name is awesome!'), AIMessage(content = 'Thanks!')]}, tokenizer = tokenizer)
   print(txt)
