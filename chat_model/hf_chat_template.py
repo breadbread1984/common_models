8e0441a14c8b006b8df6fa960f99b79ad3ecc235
breadbread1984 <breadbread1984@163.com>
2024-11-12 10:59:51 +0800

    update

--- chat_model/hf_chat_template.py
+++ chat_model/hf_chat_template.py
@@ -5,15 +5,15 @@ from collections.abc import Sequence
 from transformers import AutoTokenizer
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+from langchain_core.prompt_values import ChatPromptValue, PromptValue
 
 # NOTE: langchain-core >= 0.3.15
 
-class HFChatPromptTemplate(ChatPromptTemplate):
+class HFChatPromptValue(ChatPromptValue):
   tokenizer: Any = None
-  def format(self, **kwargs) -> str:
-    messages = self.format_messages(**kwargs)
+  def to_string(self) -> str:
     hf_messages = []
-    for m in messages:
+    for m in self.messages:
       if isinstance(m, HumanMessage):
         role = 'user'
       elif isinstance(m, AIMessage):
@@ -24,6 +24,15 @@ class HFChatPromptTemplate(ChatPromptTemplate):
         raise Exception(f'Got unsupported message type: {m}')
       hf_messages.append({'role': role, 'content': m.content})
     return self.tokenizer.apply_chat_template(hf_messages, tokenize = False, add_generation_prompt = True)
+
+class HFChatPromptTemplate(ChatPromptTemplate):
+  tokenizer: Any = None
+  def format_prompt(self, **kwargs: Any) -> PromptValue:
+    messages = self.format_messages(**kwargs)
+    return HFChatPromptValue(messages = messages, tokenizer = self.tokenizer)
+  async def aformat_prompt(self, **kwargs: Any) -> PromptValue:
+    messages = await self.format_messages(**kwargs)
+    return HFChatPromptValue(messages = messages, tokenizer = self.tokenizer)
 
 if __name__ == "__main__":
   messages = [
