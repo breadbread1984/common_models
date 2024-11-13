c950f3d2b26a17b2baa72be8db882e79a58ebdf5
breadbread1984 <breadbread1984@163.com>
2024-11-13 15:43:22 +0800

    update

--- image_generation/datasets.py
+++ image_generation/datasets.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
+from torch.utils.data import random_split
 from torchvision.datasets import CIFAR10
 from torchvision.transforms import transforms
 
-def load_datasets():
+def load_datasets(train_val_split = (45000,5000)):
   transforms = transforms.Compose([
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize([0.5], [0.5])
   ])
-  trainset = CIFAR10(root = './cifar10', train = True, download = True, transform = transforms)
-  valset = CIFAR10(root = './cifar10', train = False, download = True)
+  trainval = CIFAR10(root = './cifar10', train = True, download = True, transform = transforms)
+  trainset, valset = random_split(trainval, train_val_split)
   return trainset, valset
 
