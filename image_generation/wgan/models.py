858acca6a18fad58b8c7c4139f096f934c14bb72
breadbread1984 <breadbread1984@163.com>
2024-11-14 16:31:44 +0800

    update

---
+++ image_generation/wgan/models.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import numpy as np
+import torch
+from torch import nn
+
+class Generator(nn.Module):
+  def __init__(self, img_size, latent_dim = 100):
+    super(Generator, self).__init__()
+    def block(in_feat, out_feat, normalize = True):
+      layers = [nn.Linear(in_feat, out_feat)]
+      if normalize:
+        layers.append(nn.BatchNorm1d(out_feat, 0.8))
+      layers.append(nn.LeakyReLU(0.2, inplace = True))
+      return layers
+    self.model = nn.Sequential(
+      *block(latent_dim, 128, normalize = False),
+      *block(128,256),
+      *block(256,512),
+      *block(512, 1024),
+      nn.Linear(1024, int(np.prod([3, img_size, img_size]))),
+      nn.Tanh()
+    )
+  def forward(self, z):
+    img = self.model(z)
+    img = torch.reshape(img, (img.shape[0], 3, img_size, img_size))
+    return img
+
+class Discriminator(nn.Module):
+  def __init__(self, img_size):
+    super(Discriminator, self).__init__()
+    self.model = nn.Sequential(
+      nn.Linear(int(np.prod([3, img_size, img_size])), 512),
+      nn.LeakyReLU(0.2, inplace=True),
+      nn.Linear(512, 256),
+      nn.LeakyReLU(0.2, inplace=True),
+      nn.Linear(256, 1),
+    )
+  def forward(self, img):
+    img_flat = torch.reshape(img, (img.shape[0], -1))
+    validity = self.model(img_flat)
+    return validity
+
