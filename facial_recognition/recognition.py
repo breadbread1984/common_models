eec6629781ba72eef7802229b37a170bfab1f7ec
breadbread1984 <breadbread1984@163.com>
2024-11-15 09:33:12 +0800

    update

--- facial_recognition/recognition.py
+++ facial_recognition/recognition.py
@@ -14,9 +14,8 @@ class Recognition(object):
   def __init__(self, device = 'cuda'):
     self.db = QuantizedDB.create(hidden_dim = 512, dist = 'l2')
     self.mtcnn = MTCNN(image_size = 160, margin = 0, min_face_size = 20,
-                       thresholds=[0.6, 0.7, 0.7], factor=0.709, post_process=True,
                        device = device).eval()
-    self.resnet = InceptionResnetV1(classify = False, pretrained = 'vggface2').to(device).eval()
+    self.resnet = InceptionResnetV1(pretrained = 'vggface2').to(device).eval()
     self.device = device
     self.labels = None
   def test_celeba(self, batch_size = 1024):
