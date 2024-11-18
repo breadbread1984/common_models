f0588391b7f5837e17896eb9ba26ba65ed33374b
breadbread1984 <breadbread1984@163.com>
2024-11-18 17:26:01 +0800

    update

--- image_generation/ddpm/models.py
+++ image_generation/ddpm/models.py
@@ -14,18 +14,22 @@ class Diffusion(nn.Module):
       in_channels = in_channels,
       out_channels = out_channels,
       layers_per_block = 2,
-      block_out_channels = (128, 256, 256, 256),
+      block_out_channels = (128, 128, 256, 256, 512, 512),
       down_block_types=(
+        "DownBlock2D",
+        "DownBlock2D",
+        "DownBlock2D",
         "DownBlock2D",
         "AttnDownBlock2D",
         "DownBlock2D",
-        "DownBlock2D"
       ),
       up_block_types=(
         "UpBlock2D",
-        "UpBlock2D",
         "AttnUpBlock2D",
-        "UpBlock2D"
+        "UpBlock2D",
+        "UpBlock2D",
+        "UpBlock2D",
+        "UpBlock2D",
       ),
     )
     self.noise_scheduler = DDIMScheduler(num_train_timesteps = timesteps)
