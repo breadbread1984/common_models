0e0c91b2f31d43be74b3e805e7932d39b178685c
breadbread1984 <breadbread1984@163.com>
2024-11-19 12:44:12 +0800

    update

--- facial_recognition/train.py
+++ facial_recognition/train.py
@@ -8,47 +8,36 @@ from torch.utils.data import DataLoader
 from torch import optim
 from torch.optim.lr_scheduler import MultiStepLR
 from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data import Dataset
 from torchvision import datasets, transforms
-from torchvision.datasets import CelebA
 import numpy as np
 
 FLAGS = flags.FLAGS
 
 def add_options():
+  flags.DEFINE_string('dataset', default = 'cropped', help = 'path to preprocessed dataset')
   flags.DEFINE_integer('batch_size', default = 32, help = 'batch size')
   flags.DEFINE_integer('epochs', default = 8, help = 'epochs')
   flags.DEFINE_integer('workers', default = 8, help = 'number of worker')
   flags.DEFINE_float('lr', default = 0.001, help = 'learning rate')
   flags.DEFINE_enum('device', default = 'cuda', enum_values = {'cpu', 'cuda'}, help = 'device to use')
 
-class CropFace(object):
-  def __init__(self, ):
-    self.mtcnn = MTCNN(
-      image_size = 160, margin = 0, min_face_size = 20,
-      thresholds = [0.6, 0.7, 0.7], factor = 0.709, post_process = True,
-      device = FLAGS.device)
-  def __call__(self, image):
-    x_aligned = self.mtcnn(image)
-    return x_aligned
-
-class Identity(object):
-  def __init__(self, unique_ids):
-    self.unique_ids = unique_ids
-  def __call__(self, label):
-    idx = torch.argmax((label == self.unique_ids).to(torch.int32), dim = 0)
-    return idx
+class CelebA(Dataset):
+  def __init__(self, root_path):
+    self.paths = list()
+    for f in os.listdir(root_path):
+      stem, ext = os.path.splitext(f)
+      if ext != '.pt': continue
+      self.paths.append(join(root_path,f))
+  def __len__(self):
+    return len(self.paths)
+  def __getitem__(self, index):
+    sample = torch.load(self.list[index])
+    return sample['image'], sample['label']
 
 def main(unused_argv):
-  trainset = CelebA(root = 'celeba', split = 'train', target_type = 'identity', download = True)
-  trans = transforms.Compose([
-    CropFace(),
-  ])
-  unique_ids = torch.unique(trainset.identity)
-  target_trans = transforms.Compose([
-    Identity(unique_ids),
-  ])
-  trainset = CelebA(root = 'celeba', split = 'train', target_type = 'identity', download = True, transform = trans, target_transform = target_trans)
-  valset = CelebA(root = 'celeba', split = 'valid', target_type = 'identity', download = True, transform = trans, target_transform = target_trans)
+  trainset = CelebA(root_path = os.path.join(FLAGS.dataset, 'train'))
+  valset = CelebA(root_path = os.path.join(FLAGS.dataset, 'valid'))
   resnet = InceptionResnetV1(
     classify = True,
     pretrained = 'vggface2',
