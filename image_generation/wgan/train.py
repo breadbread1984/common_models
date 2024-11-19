085b1d00a294b945b2165c3c076fc9ea3960bff8
breadbread1984 <breadbread1984@163.com>
2024-11-19 17:48:06 +0800

    update

--- image_generation/wgan/train.py
+++ image_generation/wgan/train.py
@@ -38,10 +38,10 @@ def compute_gradient_penalty(D, real_samples, fake_samples):
   return gradient_penalty
 
 def train_one_epoch(epoch, train_dataloader, generator, discriminator, optimizer_G, optimizer_D, tb_writer):
-  for step, (x, label) in tqdm(enumerate(train_dataloader)):
+  for step, (real_imgs, label) in tqdm(enumerate(train_dataloader)):
     # train discriminator
     optimizer_D.zero_grad()
-    z = torch.normal(mean = 0, std = 1, size = (x.shape[0], FLAGS.dim)).to(next(generator.parameters()).device)
+    z = torch.normal(mean = 0, std = 1, size = (real_imgs.shape[0], FLAGS.dim)).to(next(generator.parameters()).device)
     fake_imgs = generator(z)
     real_validity = discriminator(real_imgs)
     fake_validity = discriminator(fake_imgs)
