3daa59fcb59e2081122424f85681a1349efb5737
breadbread1984 <breadbread1984@163.com>
2024-11-19 17:41:17 +0800

    update

--- image_generation/wgan/datasets.py
+++ image_generation/wgan/datasets.py
@@ -4,11 +4,11 @@ from torchvision.datasets import CIFAR10
 from torchvision.transforms import transforms
 
 def load_datasets():
-  transforms = transforms.Compose([
+  trans = transforms.Compose([
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize([0.5], [0.5])
   ])
-  trainset = CIFAR10(root = './cifar10', train = True, download = True, transform = transforms)
+  trainset = CIFAR10(root = './cifar10', train = True, download = True, transform = trans)
   return trainset
 
