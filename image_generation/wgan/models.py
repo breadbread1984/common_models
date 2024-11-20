7c5543a9a1a8f133397d65551d5199f9d8c58cdd
breadbread1984 <breadbread1984@163.com>
2024-11-20 10:38:39 +0800

    update

--- image_generation/wgan/models.py
+++ image_generation/wgan/models.py
@@ -5,40 +5,44 @@ import torch
 from torch import nn
 
 class Generator(nn.Module):
-  def __init__(self, img_size, latent_dim = 100):
+  def __init__(self, latent_dim = 100):
     super(Generator, self).__init__()
-    def block(in_feat, out_feat, normalize = True):
-      layers = [nn.Linear(in_feat, out_feat)]
-      if normalize:
-        layers.append(nn.BatchNorm1d(out_feat, 0.8))
-      layers.append(nn.LeakyReLU(0.2, inplace = True))
-      return layers
     self.model = nn.Sequential(
-      *block(latent_dim, 128, normalize = False),
-      *block(128,256),
-      *block(256,512),
-      *block(512, 1024),
-      nn.Linear(1024, int(np.prod([3, img_size, img_size]))),
+      nn.Linear(latent_dim, 256 * 4 * 4),
+      nn.Unflatten(1, (256,4,4)),
+      nn.BatchNorm2d(256),
+      nn.LeakyReLU(0.2, inplace = True),
+      nn.ConvTranspose2d(256, 128, kernel_size = (4,4), stride = (2,2), padding = 'same'), # shape = (batch, 128, 8, 8)
+      nn.BatchNorm2d(128),
+      nn.LeakyReLU(0.2, inplace = True),
+      nn.ConvTranspose2d(128, 64, kernel_size = (4,4), stride = (2,2), padding = 'same'), # shape = (batch, 64, 16, 16)
+      nn.BatchNorm2d(64),
+      nn.LeakyReLU(0.2, inplace = True),
+      nn.ConvTranspose2d(64, 3, kernel_size = (4,4), stride = (2,2), padding = 'same'), # shape = (batch, 3, 32, 32)
       nn.Tanh()
     )
-    self.img_size = img_size
   def forward(self, z):
     img = self.model(z)
-    img = torch.reshape(img, (img.shape[0], 3, self.img_size, self.img_size))
     return img
 
 class Discriminator(nn.Module):
-  def __init__(self, img_size):
+  def __init__(self, ):
     super(Discriminator, self).__init__()
     self.model = nn.Sequential(
-      nn.Linear(int(np.prod([3, img_size, img_size])), 512),
-      nn.LeakyReLU(0.2, inplace=True),
-      nn.Linear(512, 256),
-      nn.LeakyReLU(0.2, inplace=True),
-      nn.Linear(256, 1),
+      nn.Conv2d(3, 64, kernel_size = (4,4), stride = (2,2), padding = 'same'), # shape = (batch, 64, 16, 16)
+      nn.LeakyReLU(0.2, inplace = True),
+      nn.Dropout(0.3),
+      nn.Conv2d(64, 128, kernel_size = (4,4), stride = (2,2), padding = 'same'), # shape = (batch, 128, 8, 8)
+      nn.LeakyReLU(0.2, inplace = True),
+      nn.Dropout(0.3),
+      nn.Conv2d(128, 256, kernel_size = (4,4), stride = (2,2), padding = 'same'), # shape = (batch, 256, 4, 4)
+      nn.LeakyReLU(0.2, inplace = True),
+      nn.Dropout(0.3),
+      nn.Flatten(),
+      nn.Linear(256 * 4 * 4, 1),
+      nn.Sigmoid()
     )
   def forward(self, img):
-    img_flat = torch.reshape(img, (img.shape[0], -1))
-    validity = self.model(img_flat)
+    validity = self.model(img)
     return validity
 
