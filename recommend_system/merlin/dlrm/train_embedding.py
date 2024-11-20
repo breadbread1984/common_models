96f6dcf41d2e4aed7015242c9062fdd27d0a91f4
breadbread1984 <breadbread1984@163.com>
2024-11-20 16:55:44 +0800

    update

---
+++ recommend_system/merlin/dlrm/train_embedding.py
@@ -0,0 +1,28 @@
+#!/bin/bash/python3
+
+from absl import flags, app
+from merlin.core.dispath import get_lib
+from merlin.schema.tags import Tags
+from nvtabular as nvt
+
+FLAGS = flags.FLAGS
+
+def add_options():
+  flags.DEFINE_string('dataset', default = 'dataset', help = 'path to dataset')
+
+def load_datasets(root_path):
+  train = get_lib().read_parquet(join(root_path, 'ml-1m', 'train.parquet'))
+  valid = get_lib().read_parquet(join(root_path, 'ml-1m', 'valid.parquet'))
+  # table head: userId 	movieId 	rating 	timestamp
+  train_ds = nvt.Dataset(train)
+  valid_ds = nvt.Dataset(valid)
+  return train_ds, valid_ds
+
+def main(unused_argv):
+  output = ['userId', 'movieId'] >> nvt.ops.Categorify()
+  output += ['rating'] >> nvt.ops.AddMetadata(tags = [Tags.REGRESSION, Tags.TARGET])
+  output.graph
+
+if __name__ == "__main__":
+  add_options()
+  app.run(main)
