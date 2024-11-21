55a50514abb1c6e6254316fafb074acd0b703d66
breadbread1984 <breadbread1984@163.com>
2024-11-21 17:40:32 +0800

    update

--- recommend_system/merlin/dlrm/train.py
+++ recommend_system/merlin/dlrm/train.py
@@ -2,6 +2,8 @@
 
 from absl import flags, app
 import torch
+import pytorch_lightning as pl
+from merlin.dataloader.torch import Loader
 import merlin.models.torch as mm
 from create_datasets import load_datasets
 
@@ -9,13 +11,25 @@ FLAGS = flags.FLAGS
 
 def add_options():
   flags.DEFINE_string('dataset', default = 'dataset', help = 'path to dataset')
+  flags.DEFINE_string('ckpt', default = 'ckpt', help = 'path to ckpt')
+  flags.DEFINE_float('lr', default = 1e-4, help = 'learning rate')
+  flags.DEFINE_integer('batch', default = 1024, help = 'batch size')
+  flags.DEFINE_integer('epochs', default = 5, help = 'epochs')
+  flags.DEFINE_integer('workers', default = 8, help = 'workers')
 
 def main(unused_argv):
   train_transformed, valid_transformed = load_datasets(FLAGS.dataset)
   model = mm.DLRMModel(
     train_transformed.schema,
     embedding_dim = 64,
-    bottom_block = mm.MLPBlock([128, 64]),
-    top_block = mm.MLPBlock([128, 64, 32]),
+    bottom_block = mm.MLPBlock([128, 64]), # mlp for input
+    top_block = mm.MLPBlock([128, 64, 32]), # mlp for output
     prediction_tasks = mm.RegressionTask('rating')
   )
+  trainer = pl.Trainer(enable_checkpointing = True, check_val_every_n_epoch = 1, default_root_dir = FLAGS.ckpt)
+  trainer.lr = FLAGS.lr
+  trainer.fit(model, train_dataloaders = Loader(train_transformed, batch_size = FLAGS.batch), val_dataloaders = Loader(valid_transformed, batch_size = FLAGS.batch))
+
+if __name__ == "__main__":
+  add_options()
+  app.run(main)
