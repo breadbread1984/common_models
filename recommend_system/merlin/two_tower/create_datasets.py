4ecbc7cdb094fc69b01402c57ae6ee305183eba4
breadbread1984 <breadbread1984@163.com>
2024-11-25 17:54:29 +0800

    update

--- recommend_system/merlin/two_tower/create_datasets.py
+++ recommend_system/merlin/two_tower/create_datasets.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
 
+from shutil import rmtree
 from os.path import join, exists
-import tarfile
-import pandas as pd
-from merlin.datasets.synthetic import generate_data
-from merlin.models.utils.dataset import unique_rows_by_features
+from absl import flags, app
+from merlin.datasets.ecommerce import get_aliccp
+from merlin.core.dispatch import get_lib
 from merlin.schema.tags import Tags
 
-def load_datasets(train_file_path):
-  '''
-  if not exists('train'):
-    with tarfile.open(train_file_path, 'r:gz') as tar:
-      tar.extractall(path = 'train')
-  sample_sekeleton = pd.read_csv(
-    join('train','sample_skeleton_train.csv'),
-    header = None,
-    names = ["sampleId", "clickLabel", "conversionLabel", "commonFeatureIndex", "featureNum", "featureList"])
-  common_features = pd.read_csv(
-    join('train','common_features_train.csv'),
-    header = None,
-    names = ['commonFeatureIndex', 'featureNum', 'featureList'])
-  '''
-  train_raw, valid_raw = generate_data('aliccp-raw', 100000, set_sizes = (0.7, 0.3))
-  user_features = unique_rows_by_features(train_raw, Tags.USER, Tags.USER_ID).compute().reset_index(drop=True)
-  print(user_features.head())
+FLAGS = flags.FLAGS
+
+def add_options():
+  flags.DEFINE_string('output_dir', default = 'dataset', help = 'output directory')
+
+def main(unused_argv):
+  if exists(FLAGS.output_dir): rmtree(FLAGS.output_dir)
+  get_aliccp(path = FLAGS.output_dir)
+
+def load_datasets(root_path = 'dataset'):
+  train = get_lib().read_parquet(join(root_path, 'transformed', 'train.parquet'))
+  valid = get_lib().read_parquet(join(root_path, 'transformed', 'valid.parquet'))
+  user_id_raw = []
 
 if __name__ == "__main__":
-  load_datasets(None)
+  add_options()
+  app.run(main)
+
