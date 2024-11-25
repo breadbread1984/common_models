aed402128820b5ed0f08334217d10a06e556b61d
breadbread1984 <breadbread1984@163.com>
2024-11-25 17:37:09 +0800

    update

--- recommend_system/merlin/two_tower/create_datasets.py
+++ recommend_system/merlin/two_tower/create_datasets.py
@@ -1,12 +1,29 @@
 #!/usr/bin/python3
 
+from os.path import join, exists
+import tarfile
+import pandas as pd
 from merlin.datasets.synthetic import generate_data
 from merlin.models.utils.dataset import unique_rows_by_features
 from merlin.schema.tags import Tags
 
-def load_datasets():
+def load_datasets(train_file_path):
+  '''
+  if not exists('train'):
+    with tarfile.open(train_file_path, 'r:gz') as tar:
+      tar.extractall(path = 'train')
+  sample_sekeleton = pd.read_csv(
+    join('train','sample_skeleton_train.csv'),
+    header = None,
+    names = ["sampleId", "clickLabel", "conversionLabel", "commonFeatureIndex", "featureNum", "featureList"])
+  common_features = pd.read_csv(
+    join('train','common_features_train.csv'),
+    header = None,
+    names = ['commonFeatureIndex', 'featureNum', 'featureList'])
+  '''
   train_raw, valid_raw = generate_data('aliccp-raw', 100000, set_sizes = (0.7, 0.3))
   user_features = unique_rows_by_features(train_raw, Tags.USER, Tags.USER_ID).compute().reset_index(drop=True)
+  print(user_features.head())
 
 if __name__ == "__main__":
-  load_datasets()
+  load_datasets(None)
