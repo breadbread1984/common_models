b97d49b75c8983279251d9447eb28b65c78afaa5
breadbread1984 <breadbread1984@163.com>
2024-11-25 14:09:04 +0800

    update

--- recommend_system/merlin/dlrm/create_cluster.py
+++ recommend_system/merlin/dlrm/create_cluster.py
@@ -9,7 +9,7 @@ from merlin.core.compat import pynvml_mem_size, device_mem_size
 def load_cluster(device_limit_frac = 0.7, device_pool_frac = 0.8, dask_workdir = 'workdir', dashboard_port = 8787):
   assert numba.cuda.is_available()
   cluster = LocalCUDACluster(
-    protocal = 'tcp',
+    protocol = 'tcp',
     n_workers = len(numba.cuda.gpus),
     CUDA_VISIBLE_DEVICES=','.join([str(gpu) for gpu in range(len(numba.cuda.gpus))]),
     device_memory_limit = int(device_limit_frac * len(numba.cuda.gpus)),
