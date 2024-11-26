65e203f6f9c7a08b503c2ab40b7e101b168cb9a2
breadbread1984 <breadbread1984@163.com>
2024-11-26 09:29:31 +0800

    update

--- recommend_system/merlin/two_tower/create_datasets.py
+++ recommend_system/merlin/two_tower/create_datasets.py
@@ -3,10 +3,13 @@
 from shutil import rmtree
 from os.path import join, exists
 from absl import flags, app
-from merlin.datasets.ecommerce import get_aliccp
+from merlin.datasets.ecommerce import get_aliccp, transform_aliccp
 from merlin.models.utils.dataset import unique_rows_by_features
 from merlin.core.dispatch import get_lib
 from merlin.schema.tags import Tags
+import nvtabular as nvt
+from merlin.dag.ops.subgraph import Subgraph
+import merling.models.torch as mm
 
 FLAGS = flags.FLAGS
 
@@ -22,6 +25,17 @@ def load_datasets(root_path = 'dataset'):
   valid = get_lib().read_parquet(join(root_path, 'transformed', 'valid.parquet'))
   item_features = unique_rows_by_features(train, Tags.ITEM, Tags.ITEM_ID).compute().reset_index(drop = True)
   item_features.to_parquet(join(root_path, 'data', 'item_features.parquet'))
+  # define attributes subsets
+  items = ["item_id", "item_category", "item_shop", "item_brand"] >> nvt.ops.Categorify(dtype = "int32")
+  item_id = items["item_id"] >> nvt.ops.TagAsItemID()
+  item_features = items["item_category", "item_shop", "item_brand"] >> nvt.ops.TagAsItemFeatures()
+  user_id = ["user_id"] >> nvt.ops.Categorify(dtype = "int32") >> nvt.ops.TagAsUserID()
+  user_features = ["user_shops", "user_profile", "user_group", "user_gender", "user_age", "user_consumption_2",
+                   "user_is_occupied", "user_geography", "user_intentions", "user_brands", "user_categories"] >> nvt.ops.Categorify(dtype = "int32") >> nvt.ops.TagAsUserFeatures()
+  subgraph_item = Subgraph("item", Subgraph("items_cat", items) + item_id + item_features)
+  subgraph_user = Subgraph("user", user_id + user_features)
+  targets = ["click"] >> nvt.ops.AddMetadata(tags = [Tags.BINARY_CLASSIFICATION, "target"])
+  outputs = subgraph_user + subgraph_item + targets
 
 if __name__ == "__main__":
   add_options()
