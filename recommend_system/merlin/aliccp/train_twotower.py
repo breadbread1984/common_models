59284f0d19e80318df9f800a0fc069822ce287bd
breadbread1984 <breadbread1984@163.com>
2024-11-27 16:32:21 +0800

    update

--- recommend_system/merlin/aliccp/train_twotower.py
+++ recommend_system/merlin/aliccp/train_twotower.py
@@ -15,7 +15,7 @@ def add_options():
   flags.DEFINE_string('dataset', default = 'aliccp', help = 'path to dataset')
   flags.DEFINE_string('ckpt', default = 'tt_ckpt', help = 'path to ckpt')
   flags.DEFINE_integer('batch', default = 1024 * 8, help = 'batch size')
-  flags.DEFINE_integer('epochs', default = 5, help = 'epochs')
+  flags.DEFINE_integer('epochs', default = 20, help = 'epochs')
 
 def main(unused_argv):
   train = Dataset(join(FLAGS.dataset, 'processed', 'train', '*.parquet'), part_size = "500MB")
