6d94e0d0157c7853f6f371645dbd9586e29e9ed0
breadbread1984 <breadbread1984@163.com>
2024-11-28 15:30:23 +0800

    update

--- recommend_system/merlin/aliccp/train_twotower.py
+++ recommend_system/merlin/aliccp/train_twotower.py
@@ -16,6 +16,7 @@ def add_options():
   flags.DEFINE_string('ckpt', default = 'tt_ckpt', help = 'path to ckpt')
   flags.DEFINE_integer('batch', default = 1024 * 8, help = 'batch size')
   flags.DEFINE_integer('epochs', default = 20, help = 'epochs')
+  flags.DEFINE_boolean('eval_only', default = False, help = 'whether to do evaluation only')
 
 def main(unused_argv):
   train = Dataset(join(FLAGS.dataset, 'processed', 'train', '*.parquet'), part_size = "500MB")
@@ -32,20 +33,26 @@ def main(unused_argv):
     loss = "categorical_crossentropy",
     metrics = [mm.RecallAt(10), mm.NDCGAt(10)]
   )
-  if exists(FLAGS.ckpt):
+  if not FLAGS.eval_only:
+    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+      filepath=join(FLAGS.ckpt, 'tt_ckpt'),
+      save_weights_only=True,  # 仅保存权重。如果想保存整个模型，设置为 False。
+      monitor="val_loss",  # 根据验证集 loss 保存最优模型
+      save_best_only=True,  # 只保存最优的模型
+      mode="min"
+    )
+    model.fit(train, validation_data = valid, batch_size = FLAGS.batch, epochs = FLAGS.epochs, callbacks = [checkpoint_callback])
+  else:
+    model.fit(train, batch_size = FLAGS.batch, epochs = 1, steps_per_epoch = 1)
     model.load_weights(join(FLAGS.ckpt, 'tt_ckpt'))
-  checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-    filepath=join(FLAGS.ckpt, 'tt_ckpt'),
-    save_weights_only=True,  # 仅保存权重。如果想保存整个模型，设置为 False。
-    monitor="val_loss",  # 根据验证集 loss 保存最优模型
-    save_best_only=True,  # 只保存最优的模型
-    mode="min"
-  )
-  model.fit(train, validation_data = valid, batch_size = FLAGS.batch, epochs = FLAGS.epochs, callbacks = [checkpoint_callback])
+    metrics = model.evaluate(valid, batch_size = FLAGS.batch, return_dict = True)
+    print(metrics)
+  '''
   query_tower = model.retrieval_block.query_block()
   query_tower.save(join(FLAGS.ckpt, 'query_tower'))
   item_tower = model.retrieval_block.item_block()
   item_tower.save(join(FLAGS.ckpt, 'item_tower'))
+  '''
 
 if __name__ == "__main__":
   add_options()
