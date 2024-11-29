be6276b34e9a86d6e41b5b2faec316c6023662b5
breadbread1984 <breadbread1984@163.com>
2024-11-29 17:06:18 +0800

    update

---
+++ nlp/chat_model/models.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import requests
+from langchain.llms.base import LLM
+
+class TGI(LLM):
+  url: str = None
+  headers: dict = None
+  def __init__(self, host):
+    super().__init__()
+    self.url = host
+    self.headers = {'Authorization': "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"}
+  def _call(self, prompt, stop = None, run_manager = None, **kwargs):
+    data = {"inputs": prompt, "parameters": {"temperature": 0.6, "top_p": 0.9}}
+    for i in range(10):
+      response = requests.post(self.url, headers = self.headers, json = data)
+      if response.status_code == 200:
+        break
+    else:
+      raise Exception(f'请求失败{response.status_code}')
+    return response.json()['generated_text']
+  @property
+  def _llm_type(self):
+    return "tgi"
+
+def Test():
+  class Test(LLM):
+    def _call(self, prompt, stop = None, run_manager = None, **kwargs):
+      return prompt
+    @property
+    def _llm_type(self):
+      return "test"
+  return Test()
