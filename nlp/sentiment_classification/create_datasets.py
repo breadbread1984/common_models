5ed465de4e38623e93a6ed881efdd9f20bf06f91
breadbread1984 <breadbread1984@163.com>
2024-11-29 17:24:20 +0800

    update

---
+++ nlp/sentiment_classification/create_datasets.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+from absl import flags, app
+from torchtext.datasets import IMDB
+from datasets import Dataset
+
+FLAGS = flags.FLAGS
+
+def add_options():
+  flags.DEFINE_string('output', default = 'imdb', help = 'path to output dataset')
+
+def main(unused_argv):
+  train, test = IMDB()
+  print(train)
+
+if __name__ == "__main__":
+  add_options()
+  app.run(main)
