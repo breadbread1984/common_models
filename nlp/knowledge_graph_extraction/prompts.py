580689b693457ea521bda86d1fabf2471f9d9c3d
breadbread1984 <breadbread1984@163.com>
2024-12-02 15:41:52 +0800

    update

--- nlp/knowledge_graph_extraction/prompts.py
+++ nlp/knowledge_graph_extraction/prompts.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from typing import Any, Union
+from typing import Any, Union, List, Tuple, Optional
 from transformers import AutoTokenizer
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
