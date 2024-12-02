061bd1f48f1d5841484b4082960e80853eecdd12
breadbread1984 <breadbread1984@163.com>
2024-12-02 09:04:48 +0800

    update

--- nlp/sentiment_classification/create_datasets.py
+++ nlp/sentiment_classification/create_datasets.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
+from functools import partial
 from datasets import load_dataset
 
-def tokenize_function(examples):
+def tokenize_function(tokenizer, examples):
   return tokenizer(examples['text'], padding = 'max_length', truncation = True, max_length = 512)
 
-def load_imdb():
+def load_imdb(tokenizer):
   train = load_dataset('imdb', split = 'train')
   valid = load_dataset('imdb', split = 'test')
-  train = train.map(tokenize_function, batched = True)
-  valid = valid.map(tokenize_function, batched = True)
+  train = train.map(partial(tokenize_function, tokenizer = tokenizer), batched = True)
+  valid = valid.map(partial(tokenize_function, tokenizer = tokenizer), batched = True)
   train = train.rename_column("label", "labels")
   valid = valid.rename_column("label", "labels")
   train.set_format(type = "torch", columns = ["input_ids", "attention_mask", "labels"])
@@ -17,5 +18,7 @@ def load_imdb():
   return train, valid
 
 if __name__ == "__main__":
-  train, valid = load_imdb()
+  from transformers import AutoTokenizer
+  tokenizer = AutoTokenizer.from_pretrained('google-bert/bert-based-uncased') 
+  train, valid = load_imdb(tokenizer)
   print(train)
