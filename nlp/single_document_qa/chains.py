3a0c29e0fdcbe2c6ca950d9dfe183aa3b9e4eb35
breadbread1984 <breadbread1984@163.com>
2024-12-03 17:57:12 +0800

    update

---
+++ nlp/single_document_qa/chains.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+from langchain.chains.qa_with_sources.loading import load_qa_with_sources_chain
+from prompts import map_rerank_prompt, stuff_prompt, map_reduce_prompt, refine_prompt
+
+def map_rerank_chain(tokenizer, llm, **kwargs):
+  prompt = map_rerank_prompt(tokenizer)
+  chain = load_qa_with_sources_chain(llm, chain_type = 'map_rerank', prompt = prompt, **kwargs)
+  return chain
+
+def stuff_chain(tokenizer, llm, **kwargs):
+  prompt, document_prompt = stuff_prompt(tokenizer)
+  chain = load_qa_with_sources_chain(llm, chain_type = 'stuff', prompt = prompt, document_prompt = document_prompt, **kwargs)
+  return chain
+
+def map_reduce_chain(tokenizer, llm, **kwargs):
+  question_prompt, combine_prompt, document_prompt = map_reduce_prompt(tokenizer)
+  chain = load_qa_with_sources_chain(llm, chain_type = 'map_reduce', question_prompt = question_prompt, combine_prompt = combine_prompt, document_prompt = document_prompt, **kwargs)
+  return chain
+
+def refine_chain(tokenizer, llm, **kwargs):
+  question_prompt, refine_prompt. document_prompt = refine_prompt(tokenizer)
+  chain = load_qa_with_sources_chain(llm, chain_type = 'refine', question_prompt = question_prompt, refine_prompt = refine_prompt, document_prompt = document_prompt)
+  return chain
+
+if __name__ == "__main__":
+  from transformers import AutoTokenizer
+  from models import TGI
+  llm = TGI('http://localhost:8080')
+  tokenizer = AutoTokenizer.from_pretrained('Qwen/Qwen2.5-7B-Instruct')
+  chain = map_rerank_chain(tokenizer, llm)
+  chain = stuff_chain(tokenizer, llm)
+  chain = map_reduce_chain(tokenizer, llm)
+  chain = refine_chain(tokenizer, llm)
