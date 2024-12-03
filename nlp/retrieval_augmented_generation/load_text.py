a578d8b92f6bfbe351ba19b99596e2a28f1e2050
breadbread1984 <breadbread1984@163.com>
2024-12-03 10:06:42 +0800

    update

---
+++ nlp/retrieval_augmented_generation/load_text.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+from os import walk
+from os.path import join, exists
+from tqdm import tqdm
+from absl import flags, app
+from langchain.document_loaders import UnstructuredPDFLoader, UnstructuredHTMLLoader, TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_neo4j import Neo4jVector
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from models import TGI
+
+FLAGS = flags.FLAGS
+
+def add_options():
+  flags.DEFINE_string('input_dir', default = None, help = 'path to directory')
+  flags.DEFINE_string('tgi_host', default = 'http://localhost:8080/generate', help = 'host of TGI')
+  flags.DEFINE_string('neo4j_host', default = 'bolt://localhost:7687', help = 'host')
+  flags.DEFINE_string('neo4j_user', default = 'neo4j', help = 'user name')
+  flags.DEFINE_string('neo4j_password', default = 'neo4j', help = 'password')
+  flags.DEFINE_string('neo4j_db', default = 'neo4j', help = 'database')
+
+def main(unused_argv):
+  docs = list()
+  # load
+  for root, dirs, files in tqdm(walk(FLAGS.input_dir)):
+    for f in files:
+      stem, ext = splitext(f)
+      if ext.lower() in ['.htm', '.html']:
+        loader = UnstructuredHTMLLoader(join(root, f))
+      elif ext.lower() == '.txt':
+        loader = TextLoader(join(root, f))
+      elif ext.lower() == '.pdf':
+        loader = UnstructuredPDFLoader(join(root, f), mode = 'single')
+      else:
+        raise Exception('unknown format!')
+      docs.extend(loader.load())
+  # split
+  text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 150)
+  split_docs = text_splitter.split_documents(docs)
+  embedding = HuggingFaceEmbeddings(model_name = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
+  vectordb = Neo4jVector.from_documents(
+    documents = split_docs,
+    embedding = embedding,
+    url = FLAGS.neo4j_host,
+    username = FLAGS.neo4j_user,
+    password = FLAGS.neo4j_password,
+    database = FLAGS.neo4j_db,
+    index_name = "typical_rag",
+    search_type = "hybrid",
+    pre_delete_collection = True
+  )
+
+if __name__ == "__main__":
+  add_options()
+  app.run(main)
---
+++ nlp/retrieval_augmented_generation/models.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import requests
+from langchain.llms.base import LLM
+
+class TGI(LLM):
+  url: str = None
+  headers: dict = None
+  def __init__(self, host):
+    super(TGI, self).__init__()
+    self.url = host
+    self.headers = {'Authorization': "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"}
+  def _call(self, prompt, stop = None, run_manager = None, **kwargs):
+    data = {"inputs": prompt, "parameters": {"temperature": 0.6, "top_p": 0.9, "max_new_tokens": 52207}}
+    for i in range(10):
+      response = requests.post(self.url, headers = self.headers, json = data)
+      if response.status_code == 200:
+        break
+    else:
+      raise Exception(f'请求失败{response.status_code}')
+    return response.json()['generated_text']
+  @property
+  def _llm_type(self):
+    return "tgi"
+
+if __name__ == "__main__":
+  tgi = TGI('http://localhost:8080/generate')
