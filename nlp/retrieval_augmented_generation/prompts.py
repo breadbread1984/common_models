18fe39402aab98e43c6e343f703f0a92c099cdad
breadbread1984 <breadbread1984@163.com>
2024-12-03 14:21:03 +0800

    update

--- nlp/retrieval_augmented_generation/prompts.py
+++ nlp/retrieval_augmented_generation/prompts.py
@@ -6,7 +6,6 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.prompts.chat import HumanMessagePromptTemplate
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 from langchain_core.prompt_values import ChatPromptValue, PromptValue
-from langchain.chains.conversational_retrieval.prompts import CONDENSE_QUESTION_PROMPT
 
 class HFChatPromptValue(ChatPromptValue):
   tokenizer: Any = None
@@ -33,10 +32,43 @@ class HFChatPromptTemplate(ChatPromptTemplate):
     messages = await self.format_messages(**kwargs)
     return HFChatPromptValue(messages = messages, tokenizer = self.tokenizer)
 
-def condense_question_prompt(tokenizer):
-  prompt = CONDENSE_QUESTION_PROMPT
-  chat_prompt = HFChatPromptTemplate(messages = [HumanMessagePromptTemplate(prompt = prompt)], tokenizer = tokenizer)
+def contextualize_q_prompt(tokenizer):
+  system_message = (
+    "Given a chat history and the latest user question "
+    "which might reference context in the chat history, "
+    "formulate a standalone question which can be understood "
+    "without the chat history. Do NOT answer the question, just "
+    "reformulate it if needed and otherwise return it as is."
+  )
+  chat_prompt = HFChatPromptTemplate(
+    messages = [
+      ('system', system_message),
+      MessagePlaceholder("chat_history"),
+      ('human', "{input}")
+    ],
+    tokenizer = tokenizer
+  )
   return chat_prompt
+
+def qa_system_prompt(tokenizer):
+  system_message = (
+    "You are an assistant for question-answering tasks. Use "
+    "the following pieces of retrieved context to answer the "
+    "question. If you don't know the answer, just say that you "
+    "don't know. Use three sentences maximum and keep the answer "
+    "concise."
+    "\n\n"
+    "{context}"
+  )
+  qa_prompt = HFChatPromptTemplate(
+    messages = [
+      ('system', system_message),
+      MessagesPlaceholder('chat_history'),
+      ('human', "{input}")
+    ],
+    tokenize = tokenizer
+  )
+  return qa_system_prompt
 
 if __name__ == "__main__":
   from transformers import AutoTokenizer
