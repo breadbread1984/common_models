d2c1152d070dc60f5a616e4bf04ef8c7cc4e1f29
breadbread1984 <breadbread1984@163.com>
2024-12-03 09:40:19 +0800

    update

--- nlp/knowledge_graph_extraction/config.py
+++ nlp/knowledge_graph_extraction/config.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
-node_types = ['person', 'place', 'organization', 'occupation', 'meeting']
+node_types = ['Person', 'Location', 'Organization', 'Product']
 rel_types = [
-  ('person', 'work_for', 'organization'),
-  ('person', 'lives_at', 'place'),
-  ('person', 'job_is', 'occupation'),
-  ('person', 'is_friend_of', 'person'),
-  ('meeting', 'is_at', 'place')
+  ('Person', 'CEO_of', 'Organization'),
+  ('Organization', 'Based_In', 'Location'),
+  ('Product', 'Launches_From', 'Location'),
+  ('Organization', 'Develops', 'Product'),
+  ('Organization', 'Collaborates_With', 'Organization'),
+  ('Product', 'Associated_With', 'Organization')
 ]
