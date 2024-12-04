3f8e1fb569ba36a89682d47e392ed5388246f4f4
breadbread1984 <breadbread1984@163.com>
2024-12-04 18:08:47 +0800

    update

--- nlp/graph_retrieval_augmented_generation/download_testset.py
+++ nlp/graph_retrieval_augmented_generation/download_testset.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from absl import flags, app
+from os.path import exists, join
 from wget import download
 import json
 from chains import rephrase_chain
@@ -12,18 +13,19 @@ def add_options():
   flags.DEFINE_string('output', default = 'dataset.txt', help = 'path to output path')
 
 def main(unused_argv):
-  download('https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json')
+  if not exists('train-v2.0.json'):
+    download('https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json')
   chain = rephrase_chain(FLAGS.tgi_host)
   with open('train-v2.0.json', 'r') as f:
     dataset = json.loads(f.read())
-  for sample in dataset['data']:
-    for paragraph in sample['paragraphs']:
-      for qa in paragraph['qas']:
+  with open(FLAGS.output, 'w') as f:
+    for sample in dataset['data']:
+      for paragraph in sample['paragraphs']:
+        for qa in paragraph['qas']:
           question = qa['question']
           answer = qa['answers']['text']
           sentence = chain.invoke({'question': question, 'answer': answer})
-          print(question, answer, sentence)
-          exit()
+          f.write(sentence + '\n')
 
 if __name__ == "__main__":
   add_options()
