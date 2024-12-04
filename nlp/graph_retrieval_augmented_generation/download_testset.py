72f719bd2ca93969f13dc2ebd285cc3ec8552f59
breadbread1984 <breadbread1984@163.com>
2024-12-04 18:04:13 +0800

    update

---
+++ nlp/graph_retrieval_augmented_generation/download_testset.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+from absl import flags, app
+from wget import download
+import json
+from chains import rephrase_chain
+
+FLAGS = flags.FLAGS
+
+def add_options():
+  flags.DEFINE_string('tgi_host', default = 'http://localhost:8080/generate', help = 'tgi host')
+  flags.DEFINE_string('output', default = 'dataset.txt', help = 'path to output path')
+
+def main(unused_argv):
+  download('https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json')
+  chain = rephrase_chain(FLAGS.tgi_host)
+  with open('train-v2.0.json', 'r') as f:
+    dataset = json.loads(f.read())
+  for sample in dataset['data']:
+    for paragraph in sample['paragraphs']:
+      for qas in paragraph:
+        for qa in qas['qas']:
+          question = qa['question']
+          answer = qa['answers']
+          sentence = chain.invoke({'question': question, 'answer': answer})
+          print(question, answer, sentence)
+          exit()
+
+if __name__ == "__main__":
+  add_options()
+  app.run(main)
+
