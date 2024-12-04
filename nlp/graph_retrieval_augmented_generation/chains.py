e5359f1b6660c4b7cc61ce775e997f0fd260a3f2
breadbread1984 <breadbread1984@163.com>
2024-12-04 15:08:45 +0800

    update

--- nlp/graph_retrieval_augmented_generation/chains.py
+++ nlp/graph_retrieval_augmented_generation/chains.py
@@ -3,9 +3,9 @@
 from langchain_neo4j import Neo4jGraph
 from langchain_neo4j import GraphCypherQAChain
 from models import TGI
-from prompts import qa_prompt, cypher_prompt
+from prompts import qa_prompt, cypher_prompt, fewshot_cypher_prompt
 
-def load_graph_qa_chain(tgi_host, neo4j_host, neo4j_user, neo4j_password, neo4j_db):
+def load_graph_qa_chain(tgi_host = 'http://localhost:8080', neo4j_host = 'bolt://localhost:7687', neo4j_user = 'neo4j', neo4j_password = 'neo4j', neo4j_db = 'neo4j', use_selector = False):
   tokenizer = AutoTokenizer.from_pretrained('Qwen/Qwen2.5-7B-Instruct')
   llm = TGI(tgi_host)
   graph = Neo4jGraph(url = neo4j_host, username = neo4j_user, password = neo4j_password, database = neo4j_db)
@@ -15,7 +15,8 @@ def load_graph_qa_chain(tgi_host, neo4j_host, neo4j_user, neo4j_password, neo4j_
     graph = graph,
     llm = llm,
     qa_prompt = qa_prompt(tokenizer),
-    cypher_prompt = cypher_prompt(tokenizer),
+    cypher_prompt = cypher_prompt(tokenizer) if not use_selector else \
+                    fewshot_cypher_prompt(tokenizer, use_selector, neo4j_host, neo4j_user, neo4j_password, neo4j_db),
     verbose = True,
     allow_dangerous_requests = True
   )
