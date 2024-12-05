444cdb69a362e711ab1d3afcbf992765d085c8ce
breadbread1984 <breadbread1984@163.com>
2024-12-05 10:39:27 +0800

    update

--- nlp/refine_qa_system/create_datasets.py
+++ nlp/refine_qa_system/create_datasets.py
@@ -2,29 +2,28 @@
 
 from datasets import load_dataset
 
-def format_example(examples):
-  for i, example in enumerate(examples):
-    messages = list()
-    messages.append({'role': 'system', 'content': '请根据提供的文档和问题，生成简洁明了的答案。你需要从多个文档中提取关键信息，并整合这些信息来回答问题。同时，提供关键句子作为支持证据。'})
-    # format user message
-    user_message = f"问题:{example['question']}\n\n上下文：\n"
-    for index, (title, sentences) in enumerate(zip(example['context']['title'], example['context']['sentences'])):
-      user_message += f"文档{index}: {title}\n\n"
-      for sent_id, sentence in enumerate(sentences):
-        user_message += f"\t句子{sent_id}: {sentence}\n\n"
-    messages.append({'role': 'user', 'content': user_message})
-    # format assistance message
-    assistant_message = f"答案:{example['answer']}\n\n关键句子:"
-    assistant_message += ','.join([f"文档{example['context']['title'].index(title)}句子{sent_id}" for sent_id, title in zip(example['supporting_facts']['sent_id'], example['supporting_facts']['title'])])
-    messages.append({'role': 'assistant', 'content': assistant_message})
-  examples['messages'] = messages
+def format_example(example):
+  messages = list()
+  messages.append({'role': 'system', 'content': '请根据提供的文档和问题，生成简洁明了的答案。你需要从多个文档中提取关键信息，并整合这些信息来回答问题。同时，提供关键句子作为支持证据。'})
+  # format user message
+  user_message = f"问题:{example['question']}\n\n上下文：\n"
+  for index, (title, sentences) in enumerate(zip(example['context']['title'], example['context']['sentences'])):
+    user_message += f"文档{index}: {title}\n\n"
+    for sent_id, sentence in enumerate(sentences):
+      user_message += f"\t句子{sent_id}: {sentence}\n\n"
+  messages.append({'role': 'user', 'content': user_message})
+  # format assistance message
+  assistant_message = f"答案:{example['answer']}\n\n关键句子:"
+  assistant_message += ','.join([f"文档{example['context']['title'].index(title)}句子{sent_id}" for sent_id, title in zip(example['supporting_facts']['sent_id'], example['supporting_facts']['title'])])
+  messages.append({'role': 'assistant', 'content': assistant_message})
+  example['messages'] = messages
   return examples
 
 def load_hotpotqa():
   train = load_dataset('hotpotqa/hotpot_qa', 'distractor', split = 'train', trust_remote_code = True)
   valid = load_dataset('hotpotqa/hotpot_qa', 'distractor', split = 'validation', trust_remote_code = True)
-  train.map(format_example, batched = True)
-  valid.map(format_example, batched = True)
+  train.map(format_example)
+  valid.map(format_example)
   return train, valid
 
 if __name__ == "__main__":
