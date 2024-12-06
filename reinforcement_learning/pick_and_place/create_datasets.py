742a8c68655ae4f75e5b4f3b0db68f85c241be1c
breadbread1984 <breadbread1984@163.com>
2024-12-06 15:41:30 +0800

    update

--- reinforcement_learning/pick_and_place/create_datasets.py
+++ reinforcement_learning/pick_and_place/create_datasets.py
@@ -5,5 +5,13 @@ import gymnasium_robotics
 
 def load_fetchpickplace_env():
   gym.register_envs(gymnasium_robotics)
-  env = gym.make("FetchPickAndPlace-V3", render_mode = "human")
+  env = gym.make("FetchPickAndPlace-v3", render_mode = "human")
   return env
+
+if __name__ == "__main__":
+  env = load_fetchpickplace_env()
+  observation, info = env.reset(seed = 42)
+  # observation['observation'].shape = (25,)
+  # observation['achieved_goal'].shape = (3,)
+  # observation['desired_goal'].shape = (3,)
+  env.close()
