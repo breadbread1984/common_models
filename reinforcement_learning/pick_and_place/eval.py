537bca5c6b1260c1610075db06393f64096e5e03
breadbread1984 <breadbread1984@163.com>
2024-12-09 14:19:06 +0800

    update

--- reinforcement_learning/pick_and_place/eval.py
+++ reinforcement_learning/pick_and_place/eval.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from absl import flags, app
-from stable_baselines3 import SAC
+from stable_baselines3 import PPO
 from create_datasets import load_fetchpickplace_env
 import cv2
 
@@ -12,18 +12,18 @@ def add_options():
 
 def main(unused_argv):
   env = load_fetchpickplace_env()
-  model = SAC('MultiInputPolicy', env, verbose = 1)
+  model = PPO('MultiInputPolicy', env, verbose = 1)
   model.load(FLAGS.ckpt)
 
-  vec_env = model.get_env()
-  obs = vec_env.reset()
-  while True:
-    action, states = model.predict(obs, deterministic = True)
-    obs, reward, done, info = vec_env.step(action)
-    img = vec_env.render()[:,:,::-1]
-    cv2.imshow('pick and place', img)
-    cv2.waitKey(20)
-    if done: break
+  for i in range(10):
+    obs, info = env.reset()
+    while True:
+      action, states = model.predict(obs, deterministic = True)
+      obs, reward, done, trunc, info = env.step(action)
+      img = env.render()[:,:,::-1]
+      cv2.imshow('pick and place', img)
+      cv2.waitKey(20)
+      if done or trunc: break
   env.close()
 
 if __name__ == "__main__":
--- reinforcement_learning/pick_and_place/train.py
+++ reinforcement_learning/pick_and_place/train.py
@@ -1,19 +1,48 @@
 #!/usr/bin/python3
 
+from os import makedirs
 from absl import flags, app
-from stable_baselines3 import SAC
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.callbacks import CheckpointCallback, EvalCallback
+from stable_baselines3.common.monitor import Monitor
 from create_datasets import load_fetchpickplace_env
 
 FLAGS = flags.FLAGS
 
 def add_options():
   flags.DEFINE_string('ckpt', default = 'ckpt', help = 'path to save checkpoint')
-  flags.DEFINE_integer('train_steps', default = 1000000, help = 'total training steps')
+  flags.DEFINE_string('logdir', default = 'logs', help = 'path to log directory')
+  flags.DEFINE_integer('train_steps', default = 500000, help = 'total training steps')
 
 def main(unused_argv):
   env = load_fetchpickplace_env()
-  model = SAC("MultiInputPolicy", env, verbose = 1)
-  model.learn(total_timesteps = FLAGS.train_steps)
+  makedirs(FLAGS.logdir, exist_ok = True)
+  env = Monitor(env, FLAGS.logdir)
+  vec_env = make_vec_env(env_id, n_envs=4, monitor_dir=log_dir)
+  model = PPO(
+    "MultiInputPolicy",  # 使用多层感知机（MLP）策略
+    vec_env,      # 向量化环境
+    learning_rate = 3e-4,      # 学习率
+    n_steps = 2048,            # 每次更新时使用的环境步数
+    batch_size = 64,           # 每次梯度下降的批大小
+    n_epochs = 10,             # 每次更新的训练回合数
+    gamma = 0.99,              # 折扣因子
+    gae_lambda = 0.95,         # 广义优势估计的 lambda
+    clip_range = 0.2,          # PPO 的裁剪范围
+    verbose = 1,               # 输出训练信息
+    tensorboard_log = FLAGS.logdir  # TensorBoard 日志目录
+  )
+  checkpoint_callback = CheckpointCallback(save_freq=10000, save_path=FLAGS.logdir, name_prefix="ppo_fetch")
+  eval_callback = EvalCallback(
+    vec_env,
+    best_model_save_path = "./best_model/",
+    log_path = FLAGS.logdir,
+    eval_freq = 10000,
+    deterministic = True,
+    render = False
+  )
+  model.learn(total_timesteps = FLAGS.train_steps, callback = [checkpoint_callback, eval_callback])
   model.save(FLAGS.ckpt)
 
 if __name__ == "__main__":
