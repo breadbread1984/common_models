ff68af1893d0c4b6672d5cc0063a7d789b5224ea
breadbread1984 <breadbread1984@163.com>
2024-12-09 09:20:16 +0800

    update

--- reinforcement_learning/pick_and_place/train.py
+++ reinforcement_learning/pick_and_place/train.py
@@ -8,15 +8,18 @@ FLAGS = flags.FLAGS
 
 def add_options():
   flags.DEFINE_string('ckpt', default = 'ckpt', help = 'path to save checkpoint')
+  flags.DEFINE_integer('train_steps', default = 1000000, help = 'total training steps')
+  flags.DEFINE_integer('eval_steps', default = '1000', help = 'total evaluation steps')
 
 def main(unused_argv):
   env = load_fetchpickplace_env()
-  model = SAC("picke&place policy", env. verbose = 1)
-  model.learn(total_timesteps = 10000)
+  model = SAC("picke&place_policy", env, verbose = 1)
+  model.learn(total_timesteps = FLAGS.train_steps)
+  model.save(FLAGS.ckpt)
 
   vec_env = model.get_env()
   obs = vec_env.reset()
-  for i in range(1000):
+  for i in range(FLAGS.eval_steps):
     action, states = model.predict(obs, deterministic = True)
     obs, reward, done, info = vec_env.step(action)
     vec_env.render()
