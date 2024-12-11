a837a5ec22f84e3d965e08d29f878991c0a01bf9
breadbread1984 <breadbread1984@163.com>
2024-12-11 14:22:26 +0800

    update

--- deployment/test_torch.py
+++ deployment/test_torch.py
@@ -4,6 +4,7 @@ from absl import flags, app
 import requests
 import numpy as np
 import cv2
+import torch
 
 FLAGS = flags.FLAGS
 
@@ -11,6 +12,8 @@ def add_options():
   flags.DEFINE_string('host', default = "localhost", help = 'host')
   flags.DEFINE_integer('port', default = 8081, help = 'port')
   flags.DEFINE_string('input', default = 'pics/000035.jpy', help = 'path to picture')
+  flags.DEFINE_enum('method', default = 'local', enum_values = {'local', 'network'}, help = 'which method to use')
+  flags.DEFINE_string('model', default = 'converted.pt', help = 'path to model')
 
 def main(unused_argv):
   img = cv2.imread(FLAGS.input)
@@ -19,14 +22,20 @@ def main(unused_argv):
   img = (img / 255).astype(np.float32)
   img = np.transpose(img, (2,0,1))
   inputs = np.expand_dims(img, axis = 0)
-  data = {"inputs": {'%inputs': inputs.tolist()}}
-  response = requests.post(
-    f"http://{FLAGS.host}:{FLAGS.port}",
-    headers = {"Content-Type": "application/json"},
-    json = data
-  )
-  assert response.status_code == 200:
-  res = response.json()
+  if FLAGS.method == 'network':
+    data = {"inputs": {'%inputs': inputs.tolist()}}
+    response = requests.post(
+      f"http://{FLAGS.host}:{FLAGS.port}",
+      headers = {"Content-Type": "application/json"},
+      json = data
+    )
+    assert response.status_code == 200
+    res = response.json()
+  elif FLAGS.method == 'local':
+    model = torch.jit.load(FLAGS.model)
+    output = model.forward([torch.from_numpy(inputs)])
+  else:
+    raise Exception('error method')
   import pdb; pdb.set_trace()
 
 if __name__ == "__main__":
