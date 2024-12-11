ab21fe757a3184f77ef1b22f43d72f61bef7af51
breadbread1984 <breadbread1984@163.com>
2024-12-11 15:30:30 +0800

    update

--- deployment/deploy.py
+++ deployment/deploy.py
@@ -1,20 +1,35 @@
 #!/usr/bin/python3
 
 from absl import flags, app
-import tritonserver
+import subprocess
 
 FLAGS = flags.FLAGS
 
 def add_options():
-  flags.DEFINE_string('model', default = 'converted.trt', help = 'path to model')
+  flags.DEFINE_string('model', default = 'torch_model_repo', help = 'path to model')
   flags.DEFINE_string('host', default = '0.0.0.0', help = 'host')
   flags.DEFINE_integer('port', default = 8081, help = 'port')
 
+def search_command_path(command):
+  try:
+    result = subprocess.check_output(['which', command]).decode('utf-8').strip()
+    return result
+  except subprocess.CalledProcessError:
+    return None
+
 def main(unused_argv):
-  triton_server = tritonserver.Server(f'{FLAGS.host}:{FLAGS.port}')
-  triton_server.load_model(FLAGS.model)
-  triton_server.start()
-  triton_server.wait_for_server_ready()
+  full_path = search_command_path('tritonserver')
+  assert full_path is not None
+  process = subprocess.Popen([full_path,
+                              f"--model-repository={FLAGS.model}",
+                              f"--http-port={FLAGS.port}",
+                              f"--grpc-port={FLAGS.port+1}",
+                              f"--metrics-port={FLAGS.port+2}"])
+  try:
+    process.wait()
+  except KeyboardInterrupt:
+    print("Stopping Triton Server...")
+    process.kill()
 
 if __name__ == "__main__":
   add_options()
