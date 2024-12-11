68452292f08aaaa2a33cf20db2d950fd1a36ccce
breadbread1984 <breadbread1984@163.com>
2024-12-11 17:17:44 +0800

    update

--- deployment/convert2torchscript.py
+++ deployment/convert2torchscript.py
@@ -14,23 +14,24 @@ def add_options():
   flags.DEFINE_enum('weights_backbone', default = 'ResNet50_Weights.IMAGENET1K_V1', enum_values = {'ResNet50_Weights.IMAGENET1K_V1', 'VGG16_Weights.IMAGENET1K_FEATURES', 'MobileNet_V3_Large_Weights.IMAGENET1K_V1'}, help = 'backbone weights')
   flags.DEFINE_integer('classnum', default = 4, help = 'class number')
   flags.DEFINE_enum('type', default = 'trace', enum_values = {'trace', 'script'}, help = 'which way to generate torchscript')
+  flags.DEFINE_enum('device', default = 'cuda', enum_values = {'cpu', 'cuda'}, help = 'which device to use')
 
 class Wrapper(nn.Module):
   def __init__(self, model, backbone, num_classes, ckpt):
     super(Wrapper, self).__init__()
-    self.model = torchvision.models.get_model(model, weights_backbone = backbone, num_classes = num_classes)
-    self.model.load_state_dict(ckpt)
+    ckpt = torch.load(FLAGS.ckpt, map_location = FLAGS.device)
+    self.model = torchvision.models.get_model(model, weights_backbone = backbone, num_classes = num_classes).to(FLAGS.device)
+    self.model.load_state_dict(ckpt['model'])
   def forward(self, inputs):
     detections = self.model(inputs)
     detection = detections[0]
     return detection['boxes'], detection['scores'], detection['labels']
 
 def main(unused_argv):
-  ckpt = torch.load(FLAGS.ckpt, map_location = 'cpu')
-  model = Wrapper(FLAGS.model, backbone = FLAGS.weights_backbone, num_classes = FLAGS.classnum, ckpt = ckpt['model'])
+  model = Wrapper(FLAGS.model, backbone = FLAGS.weights_backbone, num_classes = FLAGS.classnum, ckpt = FLAGS.ckpt).to(FLAGS.device)
   model.eval()
   if FLAGS.type == 'trace':
-    example_input = torch.randn(1,3,600,800).to(torch.float32)
+    example_input = torch.randn(1,3,600,800).to(torch.float32).to(FLAGS.device)
     trace_model = torch.jit.trace(model, example_input)
     trace_model.save(FLAGS.output)
   elif FLAGS.type == 'script':
