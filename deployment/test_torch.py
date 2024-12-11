7a27ad76688ceba14e5047e8756ca50107381606
breadbread1984 <breadbread1984@163.com>
2024-12-11 17:09:10 +0800

    update

--- deployment/test_torch.py
+++ deployment/test_torch.py
@@ -27,9 +27,9 @@ def main(unused_argv):
     client = httpclient.InferenceServerClient(f"{FLAGS.host}:{FLAGS.port}")
     feeds = [httpclient.InferInput("input", inputs.shape, "FP32")]
     feeds[0].set_data_from_numpy(inputs)
-    outputs = [httpclient.InferRequestedOutput("boxes"),
-               httpclient.InferRequestedOutput("scores"),
-               httpclient.InferRequestedOutput("labels")]
+    outputs = [httpclient.InferRequestedOutput("%6593"),
+               httpclient.InferRequestedOutput("%6544"),
+               httpclient.InferRequestedOutput("%6546")]
     response = client.infer("torch_model", inputs = feeds, outputs = outputs, model_version = "1")
     boxes, scores, labels = response
   elif FLAGS.method == 'local':
