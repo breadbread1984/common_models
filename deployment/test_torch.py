4b680d9bdbedb3f086bc8f06c00b3cb723eaa133
breadbread1984 <breadbread1984@163.com>
2024-12-11 15:04:19 +0800

    update

--- deployment/test_torch.py
+++ deployment/test_torch.py
@@ -18,11 +18,11 @@ def add_options():
 
 def main(unused_argv):
   img = cv2.imread(FLAGS.input)
-  img = img[:,:,::-1]
-  img = np.ascontiguousarray(img)
-  img = (img / 255).astype(np.float32)
-  img = np.transpose(img, (2,0,1))
-  inputs = np.expand_dims(img, axis = 0)
+  inputs = img[:,:,::-1]
+  inputs = np.ascontiguousarray(inputs)
+  inputs = (inputs / 255).astype(np.float32)
+  inputs = np.transpose(inputs, (2,0,1))
+  inputs = np.expand_dims(inputs, axis = 0)
   if FLAGS.method == 'network':
     data = {"inputs": {'%inputs': inputs.tolist()}}
     response = requests.post(
@@ -34,10 +34,17 @@ def main(unused_argv):
     res = response.json()
   elif FLAGS.method == 'local':
     model = torch.jit.load(FLAGS.model, map_location = 'cpu')
-    output = model.forward(torch.from_numpy(inputs))
+    boxes, scores, labels = model.forward(torch.from_numpy(inputs))
   else:
     raise Exception('error method')
-  import pdb; pdb.set_trace()
+  # visualize
+  for box, score, label in zip(boxes, scores, labels):
+    if score < 0.5: continue
+    color = (255 if label == 0 else 0,255 if label == 1 else 0,255 if label == 2 else 0)
+    cv2.rectangle(img, tuple(box[:2].astype(np.int32).tolist()), tuple(box[2:].astype(np.int32).tolist()), color, 2, 1)
+  cv2.imwrite('output.png', img)
+  cv2.imshow('', img)
+  cv2.waitKey()
 
 if __name__ == "__main__":
   add_options()
