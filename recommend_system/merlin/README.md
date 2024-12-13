27fa741030189446e10c84b5f8f16b4ee47be308
breadbread1984 <breadbread1984@163.com>
2024-12-13 15:44:49 +0800

    update

--- recommend_system/merlin/README.md
+++ recommend_system/merlin/README.md
@@ -6,7 +6,14 @@ this project implements python3 scripts for calling merline
 
 ## Install prerequisite
 
-### Install and launch Merlin-pytorch
+### Install and launch Merlin-tensorflow if you use tensorflow implement
+
+```shell
+docker pull nvcr.io/nvidia/merlin/merlin-tensorflow:nightly
+docker run --gpus all  --rm -td -p 8888:8888 -p 8797:8787 -p 8796:8786 --ipc=host --cap-add SYS_NICE nvcr.io/nvidia/merlin/merlin-tensorflow:nightly
+```
+
+### Install and launch Merlin-pytorch if you use torch implement
 
 ```shell
 docker pull nvcr.io/nvidia/merlin/merlin-pytorch:nightly
---
+++ recommend_system/merlin/movielens/test_server.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+from absl import flags, app
+import tritonclient.http as httpclient
+from create_datasets import load_datasets
+
+FLAGS = flags.FLAGS
+
+def add_options():
+  flags.DEFINE_string('host', default = 'localhost', help = 'host')
+  flags.DEFINE_integer('port', default = 8081, help = 'port')
+  flags.DEFINE_string('dataset', default = 'dataset', help = 'path to dataset')
+
+def main(unused_argv):
+  _, valid = load_datasets(FLAGS.dataset)
+  client = httpclient.InferenceServerClient(url = f"{FLAGS.host}:{FLAGS.port}")
+  if not client.is_model_ready('executor_model'):
+    raise Exception('Model is not ready!')
+  for sample in valid:
+    import pdb; pdb.set_trace()
+
+if __name__ == "__main__":
+  add_options()
+  app.run(main)
+
