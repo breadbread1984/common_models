cd7e2091058aca40857c8689c8495401df195873
breadbread1984 <breadbread1984@163.com>
2024-12-13 17:33:55 +0800

    update

--- recommend_system/merlin/aliccp/train_dlrm.py
+++ recommend_system/merlin/aliccp/train_dlrm.py
@@ -6,6 +6,9 @@ from merlin.io.dataset import Dataset
 import merlin.models.tf as mm
 from merlin.schema import ColumnSchema
 from merlin.schema.tags import Tags
+from merlin.systems.dag import Ensemble
+from merlin.systems.dag.ops.workflow import TransformWorkflow
+from merlin.systems.dag.ops.tensorflow import PredictTensorflow
 import tensorflow as tf
 
 FLAGS = flags.FLAGS
@@ -17,32 +20,33 @@ def add_options():
   flags.DEFINE_integer('batch', default = 16 * 1024, help = 'batch size')
   flags.DEFINE_integer('epochs', default = 2, help = 'epochs')
   flags.DEFINE_enum('target', default = 'click', enum_values = {'click', 'conversion'}, help = 'which target to use')
-  flags.DEFINE_boolean('eval_only', default = False, help = 'whether to do evaluation only')
+  flags.DEFINE_string('pipeline', default = 'pipeline_tf', help = 'path to output pipeline')
 
 def main(unused_argv):
   train = Dataset(join(FLAGS.dataset, 'processed', 'train', '*.parquet'), part_size = "500MB")
   valid = Dataset(join(FLAGS.dataset, 'processed', 'valid', '*.parquet'), part_size = "500MB")
   model = mm.DLRMModel(
-    train.schema,
+    train.schema.without(['click', 'conversion']),
     embedding_dim=64,
     bottom_block=mm.MLPBlock([128, 64]),
     top_block=mm.MLPBlock([128, 64, 32]),
     prediction_tasks=mm.BinaryClassificationTask(FLAGS.target),
   )
-  model.compile(optimizer="adam", run_eagerly=False, metrics=[tf.keras.metrics.AUC()])
-  if not FLAGS.eval_only:
-    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-      filepath = join(FLAGS.ckpt, 'dlrm_ckpt'),
-      save_weights_only = True,
-      save_freq = 'epoch'
-    )
-    model.fit(train, validation_data=valid, batch_size=FLAGS.batch, epochs = FLAGS.epochs, callbacks = [checkpoint_callback])
-  else:
-    # NOTE: to initialize members of the model
-    model.fit(train, batch_size = FLAGS.batch, epochs = 1, steps_per_epoch = 1)
-    model.load_weights(join(FLAGS.ckpt, 'dlrm_ckpt'))
-    metrics = model.evaluate(valid, batch_size = FLAGS.batch, return_dict = True)
-    print(metrics)
+  optimizer = tf.keras.optimizers.Adam(FLAGS.lr)
+  model.compile(optimizer = optimizer, run_eagerly = False, metrics=[tf.keras.metrics.AUC()])
+  callbacks = [
+    tf.keras.callbacks.TensorBoard(log_dir = join(FLAGS.ckpt, 'logs')),
+    #tf.keras.callbacks.ModelCheckpoint(filepath = join(FLAGS.ckpt, 'dlrm_ckpt'), save_weights_only = True, save_freq = 'epoch')
+  ]
+  model.fit(train, validation_data=valid, batch_size=FLAGS.batch, epochs = FLAGS.epochs, callbacks = callbacks)
+  metrics = model.evaluate(valid, batch_size = FLAGS.batch, return_dict = True)
+  print(metrics)
+  # export to pipeline
+  workflow = nvt.Workflow.load('dlrm_torch.workflow')
+  workflow.remove_inputs(['click', 'conversion'])
+  pipeline = workflow.input_schema.column_names >> TransformWorkflow(workflow) >> PredictTensorflow(model)
+  ensemble = Ensemble(pipeline, workflow.input_schema)
+  ensemble.export(FLAGS.pipeline)
 
 if __name__ == "__main__":
   add_options()
