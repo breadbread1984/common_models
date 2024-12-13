cdd7c5c25d9210eacbb7c6f6af3ab4a9b8c5090a
breadbread1984 <breadbread1984@163.com>
2024-12-13 08:51:18 +0800

    update

--- recommend_system/merlin/movielens/train.py
+++ recommend_system/merlin/movielens/train.py
@@ -39,6 +39,7 @@ def main(unused_argv):
   trainer.lr = FLAGS.lr
   if not FLAGS.eval_only:
     trainer.fit(model, train_dataloaders = Loader(train_transformed, batch_size = FLAGS.batch), val_dataloaders = Loader(valid_transformed, batch_size = FLAGS.batch))
+    trainer.validate(model, Loader(valid_transformed, batch_size = FLAGS.batch))
   else:
     trainer.fit(model, train_dataloaders = Loader(train_transformed, batch_size = 1), val_dataloaders = Loader(valid_transformed, batch_size = 1), ckpt_path = 'last')
     trainer.validate(model, Loader(valid_transformed, batch_size = FLAGS.batch))
