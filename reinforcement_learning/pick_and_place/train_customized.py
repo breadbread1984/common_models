c75b333df647ffcdd004ae70e773645dc22dc34a
breadbread1984 <breadbread1984@163.com>
2025-01-02 16:45:32 +0800

    update

--- reinforcement_learning/pick_and_place/train_customized.py
+++ reinforcement_learning/pick_and_place/train_customized.py
@@ -82,7 +82,7 @@ class PPO(nn.Module):
       advantages[t] = delta + (gamma * lam * advantages[t + 1] if not dones[t] else 0)
     assert advantages.shape[0] == rewards.shape[0]
     return advantages
-  def get_values(self, states, rewards, dones, gamma = FLAGS.gamma):
+  def get_values(self, states, rewards, dones, gamma):
     assert states.shape[0] == rewards.shape[0] + 1 == dones.shape[0] + 1
     discount_cumsum = torch.zeros_like(rewards).to(next(self.parameters()).device) # discount_cumsum.shape = (len)
     discount_cumsum[-1] = rewards[-1] + (0 if dones[-1] else gamma * self.value_net(states[-1:])[0,0])
@@ -113,14 +113,13 @@ def main(unused_argv):
   for epoch in range(FLAGS.epochs):
     #for episode in tqdm(range(FLAGS.episodes), leave = False):
     for episode in range(FLAGS.episodes):
-      states, actions, logprobs, rewards, dones = list(), list(), list(), list(), list()
+      states, logprobs, rewards, dones = list(), list(), list(), list()
       obs, info = env.reset()
       states.append(obs['observation']) # s_t
       for step in range(FLAGS.max_ep_steps):
-        obs = torch.from_numpy(np.expand_dims(obs['observation'], axis = 0)).to(next(ppo.parameters()).device) # obs.shape = (1, 25)
-        action, logprob = ppo.act(obs) # action.shape = (1,1), logprob.shape = (1,1)
-        obs, reward, done, truc, info = env.step(action.detach().cpu().item()) # r_t
-        actions.append(torch.squeeze(action, dim = -1))
+        obs = torch.from_numpy(np.expand_dims(obs['observation'], axis = 0).astype(np.float32)).to(next(ppo.parameters()).device) # obs.shape = (1, 25)
+        action, logprob = ppo.act(obs) # action.shape = (1,4), logprob.shape = (1,1)
+        obs, reward, done, truc, info = env.step(action.detach().squeeze(dim = 0).cpu().numpy()) # r_t
         logprobs.append(torch.squeeze(logprob, dim = -1))
         rewards.append(reward)
         dones.append(done)
@@ -133,11 +132,10 @@ def main(unused_argv):
           # episodes are truncated to at most 300 steps
           assert len(states) == len(rewards) == len(dones)
           states.append(obs['observation']) # save s_t+1
-          states = torch.from_numpy(np.stack(states)).to(next(ppo.parameters()).device) # states.shape = (len + 1, 25)
-          actions = torch.cat(actions, dim = 0).to(next(ppo.parameters()).device) # actions.shape = (len)
+          states = torch.from_numpy(np.stack(states).astype(np.float32)).to(next(ppo.parameters()).device) # states.shape = (len + 1, 25)
           logprobs = torch.cat(logprobs, dim = 0).to(next(ppo.parameters()).device) # logprobs.shape = (len)
-          rewards = torch.from_numpy(np.array(rewards)).to(next(ppo.parameters()).device) # rewards.shape = (len)
-          dones = torch.from_numpy(np.array(dones)).to(next(ppo.parameters()).device) # rewards.shape = (len)
+          rewards = torch.from_numpy(np.array(rewards).astype(np.float32)).to(next(ppo.parameters()).device) # rewards.shape = (len)
+          dones = torch.from_numpy(np.array(dones).astype(np.float32)).to(next(ppo.parameters()).device) # rewards.shape = (len)
           true_values = ppo.get_values(states, rewards, dones, gamma = FLAGS.gamma) # true_values.shape = (len)
           pred_values = ppo.pred_values(states) # pred_values.shape = (len)
           advantages = ppo.advantages(states, rewards, true_values, dones, FLAGS.gamma, FLAGS.lam) # advantages.shape = (len)
