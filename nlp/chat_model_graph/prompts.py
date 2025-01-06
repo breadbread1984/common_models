f9ec3a649ccba93ba15c83e5f62445dc96adf398
breadbread1984 <breadbread1984@163.com>
2025-01-06 16:23:59 +0800

    update

--- nlp/chat_model_graph/prompts.py
+++ nlp/chat_model_graph/prompts.py
@@ -4,12 +4,42 @@ from typing import Any, Union
 from transformers import AutoTokenizer
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
-from langchain_core.prompt_values import ChatPromptValue
+from langchain_core.prompt_values import ChatPromptValue, PromptValue
+
+# NOTE: langchain-core >= 0.3.15
+
+class HFChatPromptValue(ChatPromptValue):
+  tokenizer: Any = None
+  def to_string(self) -> str:
+    hf_messages = []
+    for m in self.messages:
+      if isinstance(m, HumanMessage):
+        role = 'user'
+      elif isinstance(m, AIMessage):
+        role = 'assistant'
+      elif isinstance(m, SystemMessage):
+        role = 'system'
+      else:
+        raise Exception(f'Got unsupported message type: {m}')
+      hf_messages.append({'role': role, 'content': m.content})
+    return self.tokenizer.apply_chat_template(hf_messages, tokenize = False, add_generation_prompt = True)
+
+class HFChatPromptTemplate(ChatPromptTemplate):
+  tokenizer: Any = None
+  def format_prompt(self, **kwargs: Any) -> PromptValue:
+    messages = self.format_messages(**kwargs)
+    return HFChatPromptValue(messages = messages, tokenizer = self.tokenizer)
+  async def aformat_prompt(self, **kwargs: Any) -> PromptValue:
+    messages = await self.format_messages(**kwargs)
+    return HFChatPromptValue(messages = messages, tokenizer = self.tokenizer)
+
+from transformers import AutoTokenizer
 
 def get_prompt():
-  template = ChatPromptTemplate([
+  tokenizer = AutoTokenizer.from_pretrained('meta-llama/Llama-3.2-3B-Instruct')
+  template = HFChatPromptTemplate([
     ('system', '你是一个海盗，用海盗应该有的腔调回复用户的任何问题。'),
     MessagesPlaceholder('chat_history'),
     ('human', '{input}')
-  ])
+  ], tokenizer = tokenizer)
   return template
