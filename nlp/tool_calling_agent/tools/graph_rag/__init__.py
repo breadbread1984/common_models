2a636d14ee2512fc786d4be09aa709e8c7579a74
breadbread1984 <breadbread1984@163.com>
2025-01-08 15:23:40 +0800

    update

---
+++ nlp/tool_calling_agent/tools/graph_rag/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from .chain import graph_rag_chain
+from .config import (
+  tool_name,
+  tool_description,
+  input_description,
+  output_description,
+  neo4j_host,
+  neo4j_user,
+  neo4j_password,
+  neo4j_db
+)
