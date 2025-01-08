05060de35dfcadacabca1868a9d81f0880772688
breadbread1984 <breadbread1984@163.com>
2025-01-08 10:44:01 +0800

    update

--- nlp/tool_calling_agent/tools/rag_tool.py
+++ nlp/tool_calling_agent/tools/rag_tool.py
@@ -5,9 +5,19 @@ from pydantic import BaseModel, Field
 from langchain_core.runnables import Runnable
 from langchain.tools import StructuredTool
 from langchain.callbacks.manager import CallbackManagerForToolRun
-from .rag import input_description, output_description, tool_name, tool_description, rag_chain
+from .rag import (
+  input_description,
+  output_description,
+  tool_name,
+  tool_description,
+  rag_chain,
+  neo4j_host,
+  neo4j_user,
+  neo4j_password,
+  neo4j_db
+)
 
-def load_rag():
+def load_rag(llm):
   class RAGInput(BaseModel):
     query: str = Field(description = input_description)
   class RAGOutput(BaseModel):
@@ -22,4 +32,12 @@ def load_rag():
     args_schema: Type[BaseModel] = RAGInput
     config: RAGConfig
     def _run(self, query: str, run_manager: Optional[CallbackManagerForToolRun] = None) -> RAGOutput:
-      
+      response = self.config.invoke({'input': query, 'chat_history': []})
+      return RAGOutput(answer = response['answer'])
+  chain = rag_chain(llm, neo4j_host, neo4j_user, neo4j_password, neo4j_db)
+  return RAGTool(config = RAGConfig(chain = chain))
+
+if __name__ == "__main__":
+  tool = load_rag()
+  res = tool.invoke({"query": ""})
+  print(res)
