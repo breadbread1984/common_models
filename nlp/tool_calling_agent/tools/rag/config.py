802875377101cd4fd9af735b256d94e73ade3e87
breadbread1984 <breadbread1984@163.com>
2025-01-08 10:38:46 +0800

    update

--- nlp/tool_calling_agent/tools/rag/config.py
+++ nlp/tool_calling_agent/tools/rag/config.py
@@ -4,3 +4,8 @@ tool_name = "招股说明书"
 tool_description = "当你有招股说明书相关问题，可以调用这个工具"
 input_description = "招股说明书相关的问题"
 output_description = "招股说明书相关的答案"
+
+neo4j_host = "bolt://localhost:7687"
+neo4j_user = "neo4j"
+neo4j_password = "neo4j"
+neo4j_db = "neo4j"
