9017622d8b53ce6f32ca96511cf242e0c60c7216
breadbread1984 <breadbread1984@163.com>
2025-01-08 10:44:31 +0800

    update

--- nlp/tool_calling_agent/tools/rag/__init__.py
+++ nlp/tool_calling_agent/tools/rag/__init__.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 
 from .rag_chain import rag_chain
-from .config import input_description, output_description, tool_name, tool_description
+from .config import (
+  input_description,
+  output_description,
+  tool_name,
+  tool_description,
+  neo4j_host,
+  neo4j_user,
+  neo4j_password,
+  neo4j_db
+)
