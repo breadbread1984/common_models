413f6e77b6b1557e68dc217c65111775a45264ae
breadbread1984 <breadbread1984@163.com>
2025-01-09 17:30:03 +0800

    update

--- nlp/tool_calling_agent/tests/sql_rag_test.py
+++ nlp/tool_calling_agent/tests/sql_rag_test.py
@@ -13,11 +13,11 @@ class SQLRAGTest(unittest.TestCase):
     from tools import load_sql_rag
     from langchain import SQLDatabase
     llm = Llama3_2()
-    tool = load_sql_rag(llm)
     sqlite_path = os.path.exists(os.path.join(os.path.abspath(os.path.dirname(__file__)),'Chinook_Sqlite.sqlite'))
     if not sqlite_path:
       download('https://github.com/lerocha/chinook-database/releases/download/v1.4.5/Chinook_Sqlite.sqlite', out = os.path.abspath(os.path.dirname(__file__)))
     db = SQLDatabase.from_uri(f'sqlite:///{sqlite_path}')
+    tool = load_sql_rag(llm, db)
     print(tool.invoke('List all albums released by AC/DC?'))
     print(tool.invoke('What is genre of music release by Santana?'))
     print(tool.invoke('List three most popular tracks among all play list?'))
