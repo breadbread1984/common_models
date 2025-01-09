74c46075f726fc01bae9a3bbf8f821c4219fe41c
Konquer your Mind <breadbread1984@163.com>
2025-01-09 16:42:58 +0800

    Update config.py

--- nlp/tool_calling_agent/tools/rag/config.py
+++ nlp/tool_calling_agent/tools/rag/config.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-tool_name = "招股说明书"
-tool_description = "当你有招股说明书相关问题，可以调用这个工具"
-input_description = "招股说明书相关的问题"
-output_description = "招股说明书相关的答案"
+tool_name = "世界七大奇迹问答工具"
+tool_description = "当你有关于世界七大奇迹相关问题，可以调用这个工具"
+input_description = "世界七大奇迹相关的问题"
+output_description = "世界七大奇迹相关问题的答案"
 
 neo4j_host = "bolt://localhost:7687"
 neo4j_user = "neo4j"
