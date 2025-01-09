8f36f77679f8cc3b5dad545158825dbe012a37cc
breadbread1984 <breadbread1984@163.com>
2025-01-09 10:18:55 +0800

    update

--- nlp/tool_calling_agent/models.py
+++ nlp/tool_calling_agent/models.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 from os import environ
+import random
+import string
 from langchain import hub
 from transformers import AutoTokenizer
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
@@ -10,7 +12,56 @@ from langchain.agents.output_parsers import ToolsAgentOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.messages import HumanMessage
 
-class Llama3_2(ChatHuggingFace):
+class ChatHuggingFace2(ChatHuggingFace):
+  def generate_random_sequence(self, length = 24):
+    characters = string.ascii_letters + string.digits
+    random_sequence = ''.join(random.choice(characters) for _ in range(length))
+    return random_sequence
+  def _generate(
+    self,
+    messages,
+    stop = None,
+    run_manager = None,
+    **kwargs,
+  ):
+    # ordinary LLM inference
+    llm_input = self._to_chat_prompt(messages, **kwargs)
+    llm_result = self.llm._generate(
+      prompts=[llm_input], stop=stop, run_manager=run_manager, **kwargs
+    )
+    generations = self._to_chat_result(llm_result)
+    if 'tools' in kwargs:
+      import json_repair
+      tool_calls = json_repair.loads(generations.generations[0].message.content)
+      if type(tool_calls) is dict:
+        tool_calls = [tool_calls]
+      generations.generations[0].message.content = ''
+      for call in tool_calls:
+        generations.generations[0].message.tool_calls.append({
+          'name': call['name'],
+          'args': call['parameters'],
+          'id': f'call_{self.generate_random_sequence()}'
+        })
+    return generations
+  def _to_chat_prompt(
+    self,
+    messages,
+    **kwargs,
+  ) -> str:
+    """Convert a list of messages into a prompt format expected by wrapped LLM."""
+    if not messages:
+      raise ValueError("At least one HumanMessage must be provided!")
+
+    if not isinstance(messages[-1], HumanMessage):
+      raise ValueError("Last message must be a HumanMessage!")
+
+    messages_dicts = [self._to_chatml_format(m) for m in messages]
+
+    return self.tokenizer.apply_chat_template(
+      messages_dicts, tokenize=False, add_generation_prompt=True, **kwargs
+    )
+
+class Llama3_2(ChatHuggingFace2):
   def __init__(self,):
     environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
     super(ChatHuggingFace, self).__init__(
@@ -28,38 +79,7 @@ class Llama3_2(ChatHuggingFace):
       verbose = True
     )
   
-  def _generate(
-    self,
-    messages,
-    stop = None,
-    run_manager = None,
-    **kwargs,
-  ):
-    # ordinary LLM inference
-    llm_input = self._to_chat_prompt(messages, **kwargs)
-    llm_result = self.llm._generate(
-      prompts=[llm_input], stop=stop, run_manager=run_manager, **kwargs
-    )
-    return self._to_chat_result(llm_result)
-  def _to_chat_prompt(
-    self,
-    messages,
-    **kwargs,
-  ) -> str:
-    """Convert a list of messages into a prompt format expected by wrapped LLM."""
-    if not messages:
-      raise ValueError("At least one HumanMessage must be provided!")
-
-    if not isinstance(messages[-1], HumanMessage):
-      raise ValueError("Last message must be a HumanMessage!")
-
-    messages_dicts = [self._to_chatml_format(m) for m in messages]
-
-    return self.tokenizer.apply_chat_template(
-      messages_dicts, tokenize=False, add_generation_prompt=True, **kwargs
-    )
-
-class Qwen2_5(ChatHuggingFace):
+class Qwen2_5(ChatHuggingFace2):
   def __init__(self,):
     environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
     super(ChatHuggingFace, self).__init__(
@@ -73,36 +93,6 @@ class Qwen2_5(ChatHuggingFace):
       ),
       tokenizer = AutoTokenizer.from_pretrained('Qwen/Qwen2.5-7B-Instruct'),
       verbose = True
-    )
-  def _generate(
-    self,
-    messages,
-    stop = None,
-    run_manager = None,
-    **kwargs,
-  ):
-    # ordinary LLM inference
-    llm_input = self._to_chat_prompt(messages, **kwargs)
-    llm_result = self.llm._generate(
-      prompts=[llm_input], stop=stop, run_manager=run_manager, **kwargs
-    )
-    return self._to_chat_result(llm_result)
-  def _to_chat_prompt(
-    self,
-    messages,
-    **kwargs,
-  ) -> str:
-    """Convert a list of messages into a prompt format expected by wrapped LLM."""
-    if not messages:
-      raise ValueError("At least one HumanMessage must be provided!")
-
-    if not isinstance(messages[-1], HumanMessage):
-      raise ValueError("Last message must be a HumanMessage!")
-
-    messages_dicts = [self._to_chatml_format(m) for m in messages]
-
-    return self.tokenizer.apply_chat_template(
-      messages_dicts, tokenize=False, add_generation_prompt=True, **kwargs
     )
 
 if __name__ == "__main__":
