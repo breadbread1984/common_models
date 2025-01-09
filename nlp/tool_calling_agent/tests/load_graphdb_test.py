c3d419c499492aa185a0b95d3f3ecdc612fe69d7
breadbread1984 <breadbread1984@163.com>
2025-01-09 14:33:05 +0800

    update

--- nlp/tool_calling_agent/tests/load_graphdb_test.py
+++ nlp/tool_calling_agent/tests/load_graphdb_test.py
@@ -9,7 +9,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 class LoadGraphDBTest(unittest.TestCase):
   def test_function(self):
-    subprocess.call(['python3', os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')), 'load_graphdb.py'), '--input_dir', join(os.path.abspath(os.path.dirname(__file__)), 'graphdb_test')])
+    subprocess.call(['python3', os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')), 'load_graphdb.py'), '--input_dir', os.path.join(os.path.abspath(os.path.dirname(__file__)), 'graphdb_test'), '--split'])
 
 if __name__ == "__main__":
   unittest.main()
