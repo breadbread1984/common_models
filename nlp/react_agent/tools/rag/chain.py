3de5c99a2f54f5b36403a036af0faddf7ad221e4
breadbread1984 <breadbread1984@163.com>
2025-01-14 16:14:20 +0800

    update

--- nlp/react_agent/tools/rag/chain.py
+++ nlp/react_agent/tools/rag/chain.py
@@ -6,7 +6,6 @@ from langchain.chains import create_history_aware_retriever, create_retrieval_ch
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_neo4j import Neo4jVector
-from .config import rank
 
 def rag_chain(llm, neo4j_host, neo4j_user, neo4j_password, neo4j_db, k = 7):
   embedding = HuggingFaceEmbeddings(model_name = "intfloat/multilingual-e5-base")
@@ -14,11 +13,6 @@ def rag_chain(llm, neo4j_host, neo4j_user, neo4j_password, neo4j_db, k = 7):
   retriever = vectordb.as_retriever(search_kwargs = {"k": k})
   # chain to summarize chat history into a standalone question
   history_aware_retriever = create_history_aware_retriever(llm, retriever, hub.pull("langchain-ai/chat-langchain-rephrase"))
-  # filter documents with rank
-  def filter_results(x):
-    filtered = [doc for doc in x if doc.metadata['classification'] <= rank]
-    return filtered
-  history_aware_retriever = history_aware_retriever | filter_results
   # chain to answer the question based on retrievaled context
   combine_docs_chain = create_stuff_documents_chain(llm, hub.pull("langchain-ai/retrieval-qa-chat"))
   chain = create_retrieval_chain(history_aware_retriever, combine_docs_chain)
