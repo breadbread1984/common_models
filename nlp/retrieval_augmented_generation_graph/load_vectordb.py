a6e318f682df3b2467fad072911edc8222440ffa
breadbread1984 <breadbread1984@163.com>
2025-01-20 09:46:21 +0800

    update

--- nlp/retrieval_augmented_generation_graph/load_vectordb.py
+++ nlp/retrieval_augmented_generation_graph/load_vectordb.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 
-from os import environ, walk
+from shutil import rmtree
+from os import environ, walk, mkdir
 from os.path import join, exists, splitext
 from tqdm import tqdm
 from absl import flags, app
+import json
+from urllib.parse import urlparse
+from uuid import uuid4
+from wget import download
+import numpy as np
 from langchain.document_loaders import UnstructuredPDFLoader, UnstructuredHTMLLoader, TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_neo4j import Neo4jVector
@@ -14,10 +20,12 @@ FLAGS = flags.FLAGS
 
 def add_options():
   flags.DEFINE_string('input_dir', default = None, help = 'path to directory')
+  flags.DEFINE_string('input_json', default = None, help = 'path to json')
   flags.DEFINE_integer('length', default = 150, help = 'segment length after splitting')
   flags.DEFINE_integer('overlap', default = 10, help = 'segment overlapping length')
 
 def main(unused_argv):
+  environ['OCR_AGENT'] = 'tesseract'
   embedding = HuggingFaceEmbeddings(model_name = "intfloat/multilingual-e5-base")
   vectordb = Neo4jVector(
     embedding = embedding,
@@ -36,25 +44,56 @@ def main(unused_argv):
     vectordb.create_new_index()
   # load
   text_splitter = RecursiveCharacterTextSplitter(separators = [r"\n\n", r"\n", r"\.(?![0-9])|(?<![0-9])\.", r"。"], is_separator_regex = True, chunk_size = FLAGS.length, chunk_overlap = FLAGS.overlap)
-  for root, dirs, files in tqdm(walk(FLAGS.input_dir)):
-    for f in files:
-      stem, ext = splitext(f)
-      if ext.lower() in ['.htm', '.html']:
-        loader = UnstructuredHTMLLoader(join(root, f))
-      elif ext.lower() == '.txt':
-        loader = TextLoader(join(root, f))
-      elif ext.lower() == '.pdf':
-        loader = UnstructuredPDFLoader(join(root, f), mode = 'single')
-      else:
-        raise Exception('unknown format!')
-      docs = loader.load()
-      split_docs = text_splitter.split_documents(docs)
-      for doc in split_docs:
-        doc.metadata['url'] = f'file://{join(root, f)}' # NOTE: psuedo document url
-        import numpy as np
-        doc.metadata['classification'] = np.random.randint(low = 0, high = 3) # NOTE: psuedo document classification
-      split_docs = [doc for doc in split_docs if len(doc.page_content) > 3]
-      vectordb.add_documents(split_docs)
+  choices = np.array(['admin', 'users'])
+  if FLAGS.input_dir:
+    for root, dirs, files in tqdm(walk(FLAGS.input_dir)):
+      for f in files:
+        stem, ext = splitext(f)
+        if ext.lower() in ['.htm', '.html']:
+          loader = UnstructuredHTMLLoader(join(root, f))
+        elif ext.lower() == '.txt':
+          loader = TextLoader(join(root, f))
+        elif ext.lower() == '.pdf':
+          loader = UnstructuredPDFLoader(join(root, f), mode = 'single', strategy = 'hi_res', languages = ['en', 'zh-cn', 'zh-tw'])
+        else:
+          raise Exception('unknown format!')
+        docs = loader.load()
+        split_docs = text_splitter.split_documents(docs)
+        for doc in split_docs:
+          doc.metadata['url'] = f'file://{join(root, f)}' # NOTE: psuedo document url
+          doc.metadata['classification'] = np.random.choice(choices) # NOTE: psuedo document classification
+        split_docs = [doc for doc in split_docs if len(doc.page_content) > 3]
+        vectordb.add_documents(split_docs)
+  elif FLAGS.input_json:
+    if exists("tmp"): rmtree("tmp")
+    mkdir("tmp")
+    with open(FLAGS.input_json, 'r') as f:
+      all_pdfs = json.loads(f.read())
+    for categories, pdfs in all_pdfs.items():
+      for key, url in pdfs.items():
+        parsed_url = urlparse(url)
+        f = parsed_url.path.split('/')[-1]
+        try:
+          download(url, out = join('tmp', f))
+        except:
+          continue
+        stem, ext = splitext(f)
+        if ext.lower() in ['.htm', '.html']:
+          loader = UnstructuredHTMLLoader(join('tmp', f))
+        elif ext.lower() == '.txt':
+          loader = TextLoader(join('tmp', f))
+        elif ext.lower() == '.pdf':
+          loader = UnstructuredPDFLoader(join('tmp', f), mode = 'single', strategy = 'hi_res', languages = ['en', 'zh-cn', 'zh-tw'])
+        else:
+          print('unknown file type!')
+          rmtree(join('tmp', f))
+          continue
+        docs = loader.load()
+        split_docs = text_splitter.split_documents(docs)
+        vectordb.add_documents(split_docs)
+        rmtree(join('tmp', f))
+  else:
+    raise Exception('either input_dir or input_json must be provided!')
 
 if __name__ == "__main__":
   add_options()
